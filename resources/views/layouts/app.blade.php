f32b2dba3f4faf40834218aa7f1d66caa405f03b
Katie Arriaga M <katiearriaga@live.com>
2019-02-19 18:58:42 -0600

    Login view

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,8 @@
 <html>
     <head>
    		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        @yield('meta')
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
