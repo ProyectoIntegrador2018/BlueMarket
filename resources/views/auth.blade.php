dc086941094ed4a4692e94973869ef8a4d90e0b3
Hector Rincon <hecerinc@hotmail.com>
2019-02-21 13:56:48 -0600

    Add google library for server validation of token. Missing: front-end validation and integration to register page

--- resources/views/auth.blade.php
+++ resources/views/auth.blade.php
@@ -11,12 +11,68 @@
 <body>
 	<div class="ui container" style="margin-top: 4%">
 		<h1>Google auth</h1>
-		<div class="g-signin2" data-theme="light" data-onsuccess="onSignIn" data-longtitle="true"></div>
+		{{-- <div class="g-signin2" data-theme="light" data-onsuccess="onSignIn" data-longtitle="true"></div> --}}
+		<div id="my-signin2"></div>
 		<a href="#" class="ui primary button" style="margin-top: 40px;" onclick="signOut();">Sign out</a>
-
+		<form action="/googleauth" method="POST" id="gform">
+			@csrf
+			<p>Your token goes here</p>
+			<input type="hidden" name="id_token" id="tokenInput">
+			<button type="submit">Submit</button>
+		</form>
 	</div>
+	<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
 	<script src="https://apis.google.com/js/platform.js" async defer></script>
 	<script>
+		$(function() {
+
+			// Test content blocking on firefox:
+			let $img = $('<img/>').attr("src", "https://apps.facebook.com/favicon.ico");
+			$img.on('load', function(e){
+				loadPage(true);
+			});
+			$img.on('error', function() {
+				loadPage(false);
+			});
+			$img.css("display", "none").appendTo(document.body);
+		});
+		function loadPage(isContentBlockingEnabled) {
+			if(!isContentBlockingEnabled) {
+				let msg = 'Please disable content blocking on this page to be able to sign in.';
+				console.error(msg);
+				$('#my-signin2').html(msg);
+				return false;
+			}
+			let auth2;
+			gapi.load('auth2', function() {
+				auth2 = gapi.auth2.init({
+					hosted_domain: 'itesm.mx',
+					prompt: 'select_account'
+				});
+				// let options = new gapi.auth2.SigninOptionsBuilder();
+				// options.setPrompt('select_account');
+				loadBtn();
+			});
+		}
+		function loadBtn() {
+			gapi.signin2.render('my-signin2', {
+				'width': 240,
+				'height': 50,
+				'longtitle': true,
+				'theme': 'light',
+				'onsuccess': onSignIn,
+				'onfailure': onFailure
+			});
+		}
+		function onFailure(e) {
+			// Handle the error where user does not match domain
+			if(e.error) {
+				console.error(e);
+			}
+			if(e.type && e.type === "tokenFailed") {
+				alert('Could not sign you in using this account. Please use your \@itesm.mx account');
+			}
+		}
 		function onSignIn(googleUser) {
 			var profile = googleUser.getBasicProfile();
 			console.log(profile);
@@ -24,6 +80,9 @@
 			console.log('Name: ' + profile.getName());
 			console.log('Image URL: ' + profile.getImageUrl());
 			console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+			let token = googleUser.getAuthResponse().id_token;
+			document.getElementById('tokenInput').value = token;
+			document.getElementById('gform').querySelector('p').innerHTML = 'Token ready';
 		}
 
 		function signOut() {
