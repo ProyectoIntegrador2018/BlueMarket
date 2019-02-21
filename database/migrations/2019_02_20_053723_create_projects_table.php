e2776c9651883d05ed627b5c7d39ebd5ada81095
Ana Karen Beltrán <anakarenbmak@yahoo.com>
2019-02-21 00:01:09 -0600

    Added Project Creation

---
+++ database/migrations/2019_02_20_053723_create_projects_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->text('video')->nullable();
+            $table->text('photo')->nullable();
+            $table->text('long_description')->nullable();
+            $table->text('short_description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+}
