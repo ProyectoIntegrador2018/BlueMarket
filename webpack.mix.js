7a7ddeb2319f96298058c97a572b43fdd920bde2
Hector Rincon <hecerinc@hotmail.com>
2019-02-22 19:28:16 -0600

    Fix images assets folder. Add jQuery and define scripts section in layout. Temp change projects register route

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,9 +11,7 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-.sass('resources/sass/app.scss', 'public/css', { implementation: require('node-sass') })
-.options({ processCssUrls: false });
+mix.js('resources/js/app.js', 'public/js').sass('resources/sass/app.scss', 'public/css', { implementation: require('node-sass') }).options({processCssUrls: false });
 
 mix.copyDirectory('resources/img', 'public/img');
 mix.scripts(['resources/js/inputValidation.js'], 'public/js/inputValidation.js');
