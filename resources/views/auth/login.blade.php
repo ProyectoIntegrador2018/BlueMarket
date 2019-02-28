eecab44e74f668ba6a2c7613258baae4e5765f52
Hector Rincon <hecerinc@hotmail.com>
2019-02-28 03:06:33 -0600

    Remove further unnecessary changes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,39 +8,6 @@
 @endsection
 
 @section('content')
-	<style>
-		.hidden {
-			display: none;
-		}
-	</style>
-	<div class="ui padded content container centered-container" style="width: 60%">
-		<form method="POST" action="{{ route('login') }}" class="ui form">
-			@csrf
-			<input type="hidden" name="id_token" id="token">
-			<div class="form-group row">
-				<label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-				<div class="col-md-6">
-					<input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
-
-					@if ($errors->has('email'))
-						<span class="invalid-feedback" role="alert">
-							<strong>{{ $errors->first('email') }}</strong>
-						</span>
-					@endif
-				</div>
-
-			</div>
-
-			<div class="form-group row mb-0">
-				<div class="col-md-8 offset-md-4">
-					<button type="submit" class="ui button primary" style="margin-top: 20px;">
-						{{ __('Login') }}
-					</button>
-				</div>
-			</div>
-		</form>
-	</div>
 	<div class="ui container bluemarket centered-container">
 		<div class="signInInfo">
 			<h1>Sign in</h1>
