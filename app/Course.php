0545414301ae1e6414bcb72a3f79e65af1ca9f1e
Oscar González <osdagoso@hotmail.com>
2019-03-02 19:40:11 -0600

    Code climate fixes

--- app/Course.php
+++ app/Course.php
@@ -8,19 +8,23 @@ class Course extends Model
 {
 	protected $guarded = [];
 
-	public function teachers() {
+	public function teachers()
+	{
 		return $this->belongsToMany('App\User')->join('users as usrs', 'course_user.user_id', '=', 'usrs.id')->where('users.role', config('enum.user_roles')['teacher']);
 	}
 
-	public function students() {
+	public function students()
+	{
 		return $this->belongsToMany('App\User')->join('users as usrs', 'course_user.user_id', '=', 'usrs.id')->where('users.role', config('enum.user_roles')['student']);
 	}
 
-	public function clients() {
+	public function clients()
+	{
 		return $this->belongsToMany('App\Course', 'client_supplier_course', 'supplier_id', 'client_id');
 	}
 
-	public function suppliers() {
+	public function suppliers()
+	{
 		return $this->belongsToMany('App\Course', 'client_supplier_course', 'client_id', 'supplier_id');
 	}
 }
--- database/migrations/2019_03_02_234525_create_client_supplier_course_table.php
+++ database/migrations/2019_03_02_234525_create_client_supplier_course_table.php
@@ -6,14 +6,14 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateClientSupplierCourseTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-    	Schema::create('client_supplier_course', function (Blueprint $table) {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('client_supplier_course', function (Blueprint $table) {
 			$table->increments('id');
 			$table->integer('client_id')->unsigned();
 			$table->integer('supplier_id')->unsigned();
@@ -21,16 +21,16 @@ class CreateClientSupplierCourseTable extends Migration
 			$table->foreign('client_id')->references('id')->on('courses');
 			$table->foreign('supplier_id')->references('id')->on('courses');
 			$table->timestamps();
-        });
-    }
+		});
+	}
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('client_supplier_course');
-    }
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('client_supplier_course');
+	}
 }
