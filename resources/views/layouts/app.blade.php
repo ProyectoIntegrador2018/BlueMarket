31d1d6298546e5622e0f1bf97e169bdab7ddbe11
Katie Arriaga M <katiearriaga@live.com>
2019-03-05 23:17:21 -0600

    Removed 2 changes that I forgot to remove because I'm clumsy

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,7 @@
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		@yield('head')
+		@yield('meta')
 
 		<title>Bluemarket - @yield('title')</title>
 
