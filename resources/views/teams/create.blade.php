4450424b34f6a570acf7ff0beb2d2f42e3fb43bc
Katie Arriaga M <katiearriaga@live.com>
2019-03-09 19:57:43 -0600

    Added component in view and moved scripts to mix

--- resources/views/teams/create.blade.php
+++ resources/views/teams/create.blade.php
@@ -1,15 +1,20 @@
 
 @extends('layouts.app')
 
-@section('title', 'Register a course')
+@section('title', 'Create team')
 
 @section('content')
 <div class="padded content">
-	@form([
-		'title' => 'Create team',
-		'method' => 'POST',
-		'action' => '/route'
-	])
-	@endform
+	<h1>Create team</h1>
+	<form class="ui form" method="POST" action="/route">
+		@csrf
+		@form
+		@endform
+		<!-- Submit button -->
+		<button id="send" type="submit" class="ui button primary">Create team</button>
+	</form>
 </div>
+@section('scripts')
+<script src="/js/teams.js"></script>
+@endsection
 @endsection
