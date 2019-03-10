afab964844d1be884abd314a1dd031f294edd424
Katie Arriaga M <katiearriaga@live.com>
2019-03-10 17:28:01 -0600

    Added details (success) view

---
+++ resources/views/teams/details.blade.php
@@ -0,0 +1,34 @@
+
+@extends('layouts.app')
+
+@if(isset($team))
+	@section('title', $team['name'])
+
+	@section('content')
+	<div class="padded content">
+		<h1>{{ $team['name'] }}</h1>
+		<div class="team-info">
+			<div class="image-container">
+				<img id="avatar" src="{{ $team['photo'] }}" alt="Team avatar" class="ui small image"/>
+			</div>
+			<div class="team-people">
+				<h2>People</h2>
+				<div class="team-members">
+					@if(isset($team['members']))
+						@foreach($team['members'] as $member)
+							<!-- TODO: add route to user profile -->
+							<a href="https://www.google.com">
+								<img class="ui avatar image" src="{{ $member['avatar'] }}">
+							</a>
+						@endforeach
+					@endif
+				</div>
+			</div>
+		</div>
+		<div class="team-projects">
+			<h2>Projects</h2>
+			<!-- TODO: add project cards -->
+		</div>
+	</div>
+	@endsection
+@endif
