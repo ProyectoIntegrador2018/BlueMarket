8991127130b98b0a2cb0ec075da1860310e83bf3
Melissa Treviño <mely.trevic@gmail.com>
2019-03-09 23:17:38 -0600

    Edit user works

--- resources/views/test/users/edit.blade.php
+++ resources/views/test/users/edit.blade.php
@@ -9,33 +9,28 @@
 			<div class="row">
 				{{-- @include('admin.sidebar') --}}
 
-				<div class="col-md-9">
-					<div class="card">
-						<div class="card-header">Edit user #{{ $user->id }}</div>
-						<div class="card-body">
-							<a href="{{ url('/users') }}" title="Back"><button class="btn btn-warning btn-sm"><i class="fa fa-arrow-left" aria-hidden="true"></i> Back</button></a>
-							<br />
-							<br />
+				@if ($errors->any())
+				<ul class="alert alert-danger">
+					@foreach ($errors->all() as $error)
+						<li>{{ $error }}</li>
+					@endforeach
+				</ul>
+				@endif
 
-							@if ($errors->any())
-								<ul class="alert alert-danger">
-									@foreach ($errors->all() as $error)
-										<li>{{ $error }}</li>
-									@endforeach
-								</ul>
-							@endif
+				<form method="POST" action="{{ url('/users/' . $user->id) }}" accept-charset="UTF-8" class="ui form form-horizontal" enctype="multipart/form-data">
+					{{ method_field('PATCH') }}
+					{{ csrf_field() }}
 
-							<form method="POST" action="{{ url('/users/' . $user->id) }}" accept-charset="UTF-8" class="form-horizontal" enctype="multipart/form-data">
-								{{ method_field('PATCH') }}
-								{{ csrf_field() }}
+					@include ('test.users.form', ['formMode' => 'edit'])
 
-								@include ('test.users.form', ['formMode' => 'edit'])
+				</form>
 
-							</form>
+				<a href="{{ url('/users/' . $user->id) }}" title="Back">
+					<button class="ui white button btn btn-warning btn-sm">
+						<i class="fa fa-arrow-left" aria-hidden="true"></i>Back
+					</button>
+				</a>
 
-						</div>
-					</div>
-				</div>
 			</div>
 		</div>
 	</div>
