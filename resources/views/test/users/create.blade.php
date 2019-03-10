9ae2b3eeda21c3a4f433dfd4b4f09eaefc7aec40
Melissa Treviño <mely.trevic@gmail.com>
2019-03-09 20:52:59 -0600

    Added layout to users views.

--- resources/views/test/users/create.blade.php
+++ resources/views/test/users/create.blade.php
@@ -1,33 +1,38 @@
+
 @extends('layouts.app')
 
+@section('title', 'Create user')
+
 @section('content')
-	<div class="container">
-		<div class="row">
-			{{-- @include('admin.sidebar') --}}
+	<div class="padded content">
+		<div class="container">
+			<div class="row">
+				{{-- @include('admin.sidebar') --}}
 
-			<div class="col-md-9">
-				<div class="card">
-					<div class="card-header">Create New user</div>
-					<div class="card-body">
-						<a href="{{ url('/users') }}" title="Back"><button class="btn btn-warning btn-sm"><i class="fa fa-arrow-left" aria-hidden="true"></i> Back</button></a>
-						<br />
-						<br />
+				<div class="col-md-9">
+					<div class="card">
+						<div class="card-header">Create New user</div>
+						<div class="card-body">
+							<a href="{{ url('/users') }}" title="Back"><button class="btn btn-warning btn-sm"><i class="fa fa-arrow-left" aria-hidden="true"></i> Back</button></a>
+							<br />
+							<br />
 
-						@if ($errors->any())
-							<ul class="alert alert-danger">
-								@foreach ($errors->all() as $error)
-									<li>{{ $error }}</li>
-								@endforeach
-							</ul>
-						@endif
+							@if ($errors->any())
+								<ul class="alert alert-danger">
+									@foreach ($errors->all() as $error)
+										<li>{{ $error }}</li>
+									@endforeach
+								</ul>
+							@endif
 
-						<form method="POST" action="{{ url('/users') }}" accept-charset="UTF-8" class="form-horizontal" enctype="multipart/form-data">
-							{{ csrf_field() }}
+							<form method="POST" action="{{ url('/users') }}" accept-charset="UTF-8" class="form-horizontal" enctype="multipart/form-data">
+								{{ csrf_field() }}
 
-							@include ('test.users.form', ['formMode' => 'create'])
+								@include ('test.users.form', ['formMode' => 'create'])
 
-						</form>
+							</form>
 
+						</div>
 					</div>
 				</div>
 			</div>
--- resources/views/test/users/show.blade.php
+++ resources/views/test/users/show.blade.php
@@ -1,37 +1,62 @@
+
 @extends('layouts.app')
 
+@section('title', 'Users')
+
 @section('content')
-	<div class="container">
-		<div class="row">
-			{{-- @include('admin.sidebar') --}}
+	<div class="padded content">
+		<div class="container">
+			<div class="row">
+				{{-- @include('admin.sidebar') --}}
 
-			<div class="col-md-9">
-				<div class="card">
-					<div class="card-header">user {{ $user->id }}</div>
-					<div class="card-body">
+				<div class="col-md-9">
+					<div class="card">
+						<div class="card-header">user {{ $user->id }}</div>
+						<div class="card-body">
 
-						<a href="{{ url('/users') }}" title="Back"><button class="btn btn-warning btn-sm"><i class="fa fa-arrow-left" aria-hidden="true"></i> Back</button></a>
-						<a href="{{ url('/users/' . $user->id . '/edit') }}" title="Edit user"><button class="btn btn-primary btn-sm"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</button></a>
+							<a href="{{ url('/users') }}" title="Back">
+								<button class="btn btn-warning btn-sm">
+									<i class="fa fa-arrow-left" aria-hidden="true"></i>Back
+								</button>
+							</a>
 
-						<form method="POST" action="{{ url('users' . '/' . $user->id) }}" accept-charset="UTF-8" style="display:inline">
-							{{ method_field('DELETE') }}
-							{{ csrf_field() }}
-							<button type="submit" class="btn btn-danger btn-sm" title="Delete user" onclick="return confirm(&quot;Confirm delete?&quot;)"><i class="fa fa-trash-o" aria-hidden="true"></i> Delete</button>
-						</form>
-						<br/>
-						<br/>
+							<a href="{{ url('/users/' . $user->id . '/edit') }}" title="Edit user">
+								<button class="btn btn-primary btn-sm">
+									<i class="fa fa-pencil-square-o" aria-hidden="true"></i>Edit
+								</button>
+							</a>
+							<br/>
+							<br/>
 
-						<div class="table-responsive">
-							<table class="table">
-								<tbody>
-									<tr>
-										<th>ID</th><td>{{ $user->id }}</td>
-									</tr>
-									<tr><th> Name </th><td> {{ $user->name }} </td></tr><tr><th> Email </th><td> {{ $user->email }} </td></tr><tr><th> Role </th><td> {{ $user->role }} </td></tr>
-								</tbody>
-							</table>
+							<div class="userInfo">
+								<img src="{{ $user->picture_url }}" alt="User profile picture">
+								<p> {{ $user->name }} </p>
+								<p> {{ $user->role }} </p>
+
+								<div class="table-responsive">
+										<table class="table">
+											<tbody>
+												<tr>
+													<th>ID</th>
+													<td>{{ $user->id }}</td>
+												</tr>
+												<tr>
+													<th>Email</th>
+													<td> {{ $user->email }} </td>
+												</tr>
+												<tr>
+													<th>Created</th>
+													<td> {{ $user->created_at }} </td>
+												</tr>
+												<tr>
+													<th>Last logon</th>
+													<td> {{ $user->last_logon }} </td>
+												</tr>
+											</tbody>
+										</table>
+									</div>
+							</div>
 						</div>
-
 					</div>
 				</div>
 			</div>
