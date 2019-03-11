1c3b3f5b7346eb540d8a613c2449bb22fe8893d8
marcegarzab <marcegarzab@gmail.com>
2019-03-11 16:11:55 -0600

    Created projects folder for views and started details

---
+++ resources/views/projects/details.blade.php
@@ -0,0 +1,43 @@
+<div class="padded content">
+	<form class="ui form">
+		<!-- Project Name -->
+		<!-- Project Image -->
+		<div class="projectImage field">
+			{{-- <label for="projectImage">Project Image</label> --}}
+			<img src="https://lorempixel.com/400/400" alt="Project Image" class="ui medium image" id="projectImagePreview"/>
+		</div>
+		<!-- Category -->
+		<div class="ui left aligned container">
+			<p>Category: </p>
+		</div>
+		<!-- Skillset -->
+		<div class="ui left aligned container">
+			<p>Skillset: </p>
+		</div>
+		<!-- Public Milestone -->
+		<div class="ui left aligned container">
+			<p>Public Milestone: </p>
+		</div>
+		<!-- Associated Course -->
+		<div class="ui left aligned container">
+			<p>Associated Course: </p>
+		</div>
+		<!-- Associated Team -->
+		<div class="ui left aligned container">
+			<p>Associated Team: </p>
+		</div>
+		<!-- Short Description -->
+		<div class="ui left aligned container">
+			<p>Brief Description: </p>
+		</div>
+		<!-- Long Description -->
+		<div class="ui left aligned container">
+			<p>Detailed Description: </p>
+		</div>
+		<!-- Video Pitch -->
+		<div class="ui left aligned container">
+			<p>Video Pitch: </p>
+			<iframe width="560" height="315" src="https://www.youtube.com/embed/xvwBtODV0ms" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+		</div>
+	</form>
+</div>
