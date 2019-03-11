009b2211145da1f426d7d5e394461151f354575b
Rubén de la Peña <ruben.dlpena@gmail.com>
2019-03-11 15:03:08 -0600

    Changed spaces for tabs

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,14 +10,14 @@ class UserController extends Controller
 {
 	const ROLES = 'enum.user_roles';
 
-    /**
-     * Display a listing of the resource.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\View
-     */
-    public function index(Request $request)
-    {
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return \Illuminate\View
+	 */
+	public function index(Request $request)
+	{
 		$searchQuery = $request->get('search');
 		$paginationSize = $request->get('paginationSize');
 
@@ -29,26 +29,26 @@ class UserController extends Controller
 		}
 
 		return view('admin.users.index', ['users' => $users]);
-    }
+	}
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\View
-     */
-    public function create()
-    {
-        return view('admin.users.create');
-    }
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return \Illuminate\View
+	 */
+	public function create()
+	{
+		return view('admin.users.create');
+	}
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
-     */
-    public function store(Request $request)
-    {
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+	 */
+	public function store(Request $request)
+	{
 		$validatedAttributes = request()->validate([
 			'name' => 'required|string',
 			'email' => 'required|string',
@@ -62,59 +62,59 @@ class UserController extends Controller
 		}
 
 		return redirect('users')->with('flash_message', 'User added!');
-    }
+	}
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\View
-     */
-    public function show(int $id)
-    {
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\View
+	 */
+	public function show(int $id)
+	{
 		if ($user->role != config(self::ROLES)['sys_admin']) {
 			abort(400);
 		}
 
 		$user = User::find($id);
-        return view('admin.users.show', ['user' => $user]);
-    }
+		return view('admin.users.show', ['user' => $user]);
+	}
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\View
-     */
-    public function edit(int $id)
-    {
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\View
+	 */
+	public function edit(int $id)
+	{
 		$user = User::find($id);
-        return view('admin.users.edit', ['user' => $user]);
-    }
+		return view('admin.users.edit', ['user' => $user]);
+	}
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int $id
-     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
-     */
-    public function update(Request $request, int $id)
-    {
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  int $id
+	 * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+	 */
+	public function update(Request $request, int $id)
+	{
 		$user = User::findOrFail($id);
 		$user->update($request->all());
 
 		return redirect('users')->with('flash_message', 'User updated!');
-    }
+	}
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\User  $user
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(User $user)
-    {
-        abort(401);
-    }
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  \App\User  $user
+	 * @return \Illuminate\Http\Response
+	 */
+	public function destroy(User $user)
+	{
+		abort(401);
+	}
 }
