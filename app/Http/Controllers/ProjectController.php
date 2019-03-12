4014c739c6cc1c503a7c5822ea489e9af579ff96
Hector Rincon <hecerinc@hotmail.com>
2019-03-11 21:33:29 -0600

    Allow project index

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -11,7 +11,7 @@ use Illuminate\Validation\Rule;
 class ProjectController extends Controller
 {
 	public function __construct() {
-		$this->middleware('auth');
+		$this->middleware('auth')->except('index');
 	}
 
 	public function index() {
