92033cfbb6e24f712800c78ad574025a5bff4f10
Hector Rincon <hecerinc@hotmail.com>
2019-03-13 16:42:57 -0600

    Add scaffolding for admin internal site

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	@yield('meta')
+
+	<title>Admin &raquo; @yield('title')</title>
+
+	<!-- Styles -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap-reboot.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap-grid.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
+	<link href="https://fonts.googleapis.com/css?family=Nunito:300,400,600,700" rel="stylesheet">
+	<link href="{{ mix('css/admin.css') }}" rel="stylesheet">
+
+</head>
+<body>
+	<div class="my-container">
+		<aside class="Sidebar">
+			<a href="#" class="logo">
+				<img src="img/logo.svg" alt="Bluemarket Admin Dashboard">
+			</a>
+			<hr>
+			<a href="#">Dashboard</a>
+			<hr>
+			<ul>
+				<li><a href="#">Dashboard</a></li>
+				<li><a href="#">Components</a></li>
+				<li><a href="#">Utilities</a></li>
+			</ul>
+			<hr>
+		</aside>
+		<main class="contentPane">
+			<nav class="mainNav navbar navbar-light" style="background-color: #fff">
+				<form class="form-inline">
+					<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+					<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+				</form>
+				<div class="navbar-brand">
+					<a href="#" class="user-avatar">
+						Alejandro Rodr&iacute;guez
+						<img class="user-avatar-img" src="http://colegioclassea.com.br/wp-content/themes/PageLand/assets/img/avatar/avatar.jpg" alt="User name">
+					</a>
+				</div>
+			</nav>
+			<div class="container-fluid">
+				@yield('content')
+			</div>
+		</main>
+	</div>
+</body>
+</html>
