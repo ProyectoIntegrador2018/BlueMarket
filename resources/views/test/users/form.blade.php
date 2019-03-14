1da346a32a1c2d800b7fec81d9cc2b086f4f202f
Melissa Treviño <mely.trevic@gmail.com>
2019-03-13 23:29:19 -0600

    Edited form fields (create and edit), removed submit button. Missing input validation before submit.

--- resources/views/test/users/form.blade.php
+++ resources/views/test/users/form.blade.php
@@ -1,14 +1,22 @@
-<div class="form-group {{ $errors->has('name') ? 'has-error' : ''}}">
+<!-- Name -->
+<div class="field {{ $errors->has('name') ? 'has-error' : ''}}">
 	<label for="name" class="control-label">{{ 'Name' }}</label>
 	<input class="form-control" name="name" type="text" id="name" value="{{ isset($user->name) ? $user->name : ''}}" >
 	{!! $errors->first('name', '<p class="help-block">:message</p>') !!}
 </div>
-<div class="form-group {{ $errors->has('role') ? 'has-error' : ''}}">
+
+<!-- Email -->
+<div class="field {{ $errors->has('email') ? 'has-error' : ''}}">
+	<label for="email" class="control-label">{{ 'Email' }}</label>
+	<input class="form-control" name="email" type="text" id="email" value="{{ isset($user->email) ? $user->email : ''}}" >
+	{!! $errors->first('email', '<p class="help-block">:message</p>') !!}
+</div>
+
+<!-- Role -->
+<div class="field {{ $errors->has('role') ? 'has-error' : ''}}">
 	<label for="role" class="control-label">{{ 'Role' }}</label>
 	<input class="form-control" name="role" type="number" id="role" value="{{ isset($user->role) ? $user->role : ''}}" >
 	{!! $errors->first('role', '<p class="help-block">:message</p>') !!}
 </div>
 
-<div class="form-group">
-	<input class="ui primary button btn btn-primary" type="submit" value="{{ $formMode === 'edit' ? 'Update' : 'Create' }}">
-</div>
+<br />
