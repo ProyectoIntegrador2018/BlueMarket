51bacec8f82afdbadea6dd4d07fea40588b98bdd
Melissa Treviño <mely.trevic@gmail.com>
2019-03-13 23:35:28 -0600

    View user done. Missing role handling.

--- resources/views/test/users/show.blade.php
+++ resources/views/test/users/show.blade.php
@@ -5,73 +5,58 @@
 
 @section('content')
 	<div class="padded content">
-		<div class="container">
-			<div class="row">
-				{{-- @include('admin.sidebar') --}}
+		{{-- @include('admin.sidebar') --}}
 
-				<div class="col-md-9">
+		<div class="userInfo">
+			<img class="ui avatar image" src="<?= $user->picture_url ?>" alt="User profile picture">
+			<h1> {{ $user->name }} </h1>
+			<div>
+				<!-- Aquí tengo que confirmar cómo me van a pasar el rol -->
+				@switch($user->role)
+					@case(1)
+					<span class="tag teacher">Teacher</span>
+					@break
 
-					<div class="userInfo">
-						<img class="ui avatar image" src="<?= $user->picture_url ?>" alt="User profile picture">
-						<h1> {{ $user->name }} </h1>
-						<div>
-							@switch($user->role)
-								@case(1)
-								<span class="tag student">Student</span>
-								@break
+					@case(2)
+					<span class="tag student">Student</span>
+					@break
 
-								@case(2)
-								<span class="tag teacher">Teacher</span>
-								@break
+					@case(3)
+					<span class="tag admin">Administrator</span>
+					@break
 
-								@case(3)
-								<span class="tag admin">Administrator</span>
-								@break
-
-								@default
-								<span class="tag student">Student</span>
-							@endswitch
-						</div>
-
-						<br>
-
-						<div>
-							<table class="ui single line table userInfo">
-								<tbody>
-									<tr>
-										<th>User ID</th>
-										<td> {{ $user->id }} </td>
-									</tr>
-									<tr>
-										<th>Email</th>
-										<td> {{ $user->email }} </td>
-									</tr>
-									<tr>
-										<th>Created</th>
-										<td> {{ $user->created_at }} </td>
-									</tr>
-									<tr>
-										<th>Last logon</th>
-										<td> {{ $user->last_logon }} </td>
-									</tr>
-								</tbody>
-							</table>
-						</div>
-
-						<a href="{{ url('/users') }}" title="Back">
-							<button class="ui white button btn btn-warning btn-sm">
-								<i class="fa fa-arrow-left" aria-hidden="true"></i>Back
-							</button>
-						</a>
-
-						<a href="{{ url('/users/' . $user->id . '/edit') }}" title="Edit user">
-							<button class="ui primary button btn btn-primary btn-sm">
-								<i class="fa fa-pencil-square-o" aria-hidden="true"></i>Edit
-							</button>
-						</a>
-					</div>
-				</div>
+					@default
+					<span class="tag admin">Administator</span>
+				@endswitch
 			</div>
+
+			<br>
+
+			<table class="ui single line table userInfo">
+				<tbody>
+					<tr>
+						<th>User ID</th>
+						<td> {{ $user->id }} </td>
+					</tr>
+					<tr>
+						<th>Email</th>
+						<td> {{ $user->email }} </td>
+					</tr>
+					<tr>
+						<th>Created</th>
+						<td> {{ $user->created_at }} </td>
+					</tr>
+					<tr>
+						<th>Last logon</th>
+						<td> {{ $user->last_logon }} </td>
+					</tr>
+				</tbody>
+			</table>
+
+			<button class="ui primary button" title="Edit" onclick="location.href='{{ url('/users/' . $user->id . '/edit') }}'">Edit</button>
+
+			<button class="ui white button" title="Back" onclick="location.href='{{ url('/users') }}'">Back</button>
 		</div>
+	</div>
 	</div>
 @endsection
