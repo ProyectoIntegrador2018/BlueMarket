9a362beb84ca1e27c3895836683ff8d64da0fe78
Melissa Treviño <mely.trevic@gmail.com>
2019-03-15 21:33:58 -0600

    Renamed folder to 'course'. Added course details for backend integration. Missing teachers and associated courses info.

---
+++ resources/views/course/details.blade.php
@@ -0,0 +1,34 @@
+@dd($course)
+@extends('layouts.app')
+
+@section('title', $course->name)
+
+@section('content')
+<div class="padded content">
+	<h1>{{ $course->name }}</h1>
+	<div>
+		<p class="coursekey">Your course key is <br> <code>{{ isset($course->course_key) ? $course->course_key : "ERROR" }}</code></p>
+		<h2>Overview:</h2>
+		<p><strong>Course name:</strong> {{ $course->name }}</p>
+		<p><strong>Teacher(s):</strong></p>
+		{{-- <ul>
+			@foreach ($course->$teachers as $teacher)
+			<li>{{ $teacher->name }}</li>
+			@endforeach
+		</ul> --}}
+		<p><strong>Course schedule:</strong> {{ $course->schedule }}</p>
+		<p><strong>Max. team size:</strong> {{ $course->max_team_size }}</p>
+		{{-- @if (isset($course->suppliers))
+			<p><strong>Associated courses:</strong></p>
+			<ul>
+				@foreach ($course->suppliers as $supplier)
+					<li>{{ $supplier->name }}</li>
+				@endforeach
+			</ul>
+		@endif --}}
+	</div>
+</div>
+@endsection
+
+@section('scripts')
+@endsection
