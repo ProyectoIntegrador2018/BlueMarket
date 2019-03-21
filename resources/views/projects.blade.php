0d446c57a39cc17106f2e1f4a3f69ad6c8154a45
marcegarzab <marcegarzab@gmail.com>
2019-03-21 16:33:11 -0600

    Implement search by name and tag function by Hector, still needs fixing

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -8,12 +8,12 @@
 	<h1>Projects</h1>
 	<!-- Filters -->
 	<form class="ui form">
-		<!-- Search by Name -->
+		<!-- Search by name -->
 		<div class="field">
 			<label for="searchName">Name</label>
 			<input id="searchName" type="text" name="searchName" placeholder="e.g. A cool project">
 		</div>
-		<!-- Search by Tag -->
+		<!-- Search by tag -->
 		<div class="field">
 			<label for="tags">Tags</label>
 			<select id="searchTags" name="tags" class="ui fluid search dropdown searchTags" multiple>
@@ -36,5 +36,50 @@
 @section('scripts')
 <script>
 	$('.ui.dropdown').dropdown();
+	// npm install --save lodash.debounce OR yarn add lodash.debounce:
+	import _debounce from 'lodash.debounce';
+	// Event handler (do something on type):
+	$('#searchName').on('keyup', _debounce(filterProjects2, 200));
+	$('#searchTags').on('change', filterProjects2);
+	// Flatten out the object:
+	projects.forEach((project, index) => {
+		project.name = project.name.toLowerCase();
+		project.domid = index;
+		project.tags = project.tags.map(tag => {
+			return tag.name;
+		});
+	});
+	// DOM Elements (<projectCard>):
+	const $projectCards = $('.ProjectCard-container'); // Array
+	// projects[0] => $projectCards[0]
+	// Search by name, labels, or skills
+	// Tags => labels, skills
+	function filterProjects2() {
+		// Get the seabrch name query (string)
+		// Get the search tags query
+			// AND project.tags includes labels, skills
+		const nameQuery = $('#searchName').val().trim().toLowerCase();
+		const tagQuery = $('#searchTags').val(); // Returns an array for the select
+		// Get all the projects where name LIKE %namequery%:
+		let projectResults = projects.filter(project => {
+			return project.name.indexOf(nameQuery) > -1;
+		});
+		// Get the projects that match search tags:
+		// All the projects where projects.tags contain all of tagQuery
+		const finalResults = projectResults.filter(project => {
+			let matchesAllTags = true;
+			for (var i = 0; i < tagQuery.length; i++) {
+				if(project.tags.indexOf(tagQuery[i]) === -1) {
+					matchesAllTags = false;
+					break;
+				}
+			}
+			return matchesAllTags;
+		});
+		$projectCards.hide();
+		finalResults.forEach(project => {
+			$projectCards.eq(project.domid).show();
+		});
+	}
 </script>
 @endsection
