829bd7da218f6f40e30f6646369a7f7962474072
marcegarzab <marcegarzab@gmail.com>
2019-03-25 16:49:42 -0600

    Remove Hector's search function for next PR)

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -36,50 +36,5 @@
 @section('scripts')
 <script>
 	$('.ui.dropdown').dropdown();
-	// npm install --save lodash.debounce OR yarn add lodash.debounce:
-	import _debounce from 'lodash.debounce';
-	// Event handler (do something on type):
-	$('#searchName').on('keyup', _debounce(filterProjects2, 200));
-	$('#searchTags').on('change', filterProjects2);
-	// Flatten out the object:
-	projects.forEach((project, index) => {
-		project.name = project.name.toLowerCase();
-		project.domid = index;
-		project.tags = project.tags.map(tag => {
-			return tag.name;
-		});
-	});
-	// DOM Elements (<projectCard>):
-	const $projectCards = $('.ProjectCard-container'); // Array
-	// projects[0] => $projectCards[0]
-	// Search by name, labels, or skills
-	// Tags => labels, skills
-	function filterProjects2() {
-		// Get the seabrch name query (string)
-		// Get the search tags query
-			// AND project.tags includes labels, skills
-		const nameQuery = $('#searchName').val().trim().toLowerCase();
-		const tagQuery = $('#searchTags').val(); // Returns an array for the select
-		// Get all the projects where name LIKE %namequery%:
-		let projectResults = projects.filter(project => {
-			return project.name.indexOf(nameQuery) > -1;
-		});
-		// Get the projects that match search tags:
-		// All the projects where projects.tags contain all of tagQuery
-		const finalResults = projectResults.filter(project => {
-			let matchesAllTags = true;
-			for (var i = 0; i < tagQuery.length; i++) {
-				if(project.tags.indexOf(tagQuery[i]) === -1) {
-					matchesAllTags = false;
-					break;
-				}
-			}
-			return matchesAllTags;
-		});
-		$projectCards.hide();
-		finalResults.forEach(project => {
-			$projectCards.eq(project.domid).show();
-		});
-	}
 </script>
 @endsection
