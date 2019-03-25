09cbd76ccbcb1c6a4b73acf2fa9640d75c2e0583
Hector Rincon <hecerinc@hotmail.com>
2019-03-24 22:55:56 -0600

    Add fields necessary for team - project association

---
+++ database/migrations/2019_03_24_224218_add_team_id_to_projects_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddTeamIdToProjectsTable extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::table('projects', function (Blueprint $table) {
+			$table->unsignedInteger('team_id');
+			$table->foreign('team_id')->references('id')->on('teams')->onDelete('cascade');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::table('projects', function (Blueprint $table) {
+			$table->dropColumn('team_id');
+		});
+	}
+}
