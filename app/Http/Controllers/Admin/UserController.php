08e73441b4297950ffd4a5d4d4fe2d5ad33af591
Melissa Treviño <mely.trevic@gmail.com>
2019-03-29 00:14:17 -0600

    Fix UserController files.

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -1,96 +1,105 @@
 <?php
 
-namespace App\Http\Controllers;
-
-use App\Http\Controllers\Controller;
+namespace App\Http\Controllers\Admin;
 
 use App\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Admin\AdminController;
 
-class UserController extends Controller {
+/**
+ * Admin UsersController
+ */
+class UserController extends AdminController {
+
+	public function __construct() {
+		// Add the auth
+		$this->middleware('auth');
+	}
+
 	/**
-	 * Display a listing of the resource.
+	 * Display a listing of the users.
 	 *
-	 * @return \Illuminate\View\View
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return \Illuminate\View
 	 */
 	public function index(Request $request) {
-		$keyword = $request->get('search');
-		$perPage = 25;
+		$searchQuery = $request->get('search');
+		$paginationSize = $request->get('paginationSize');
 
-		if (!empty($keyword)) {
-			$users = User::latest()->paginate($perPage);
-		} else {
-			$users = User::latest()->paginate($perPage);
+		if (!isset($paginationSize)) {
+			$paginationSize = 25;
 		}
 
-		return view('test.users.index', compact('users'));
+		if (isset($searchQuery) && !empty($searchQuery)) {
+			$users = User::where('name', 'like', "%{$searchQuery}%")->orWhere('email', 'like', "%{$searchQuery}%")->simplePaginate($paginationSize);
+		}
+		else {
+			$users = User::simplePaginate($paginationSize);
+		}
+
+		return view('admin.users.index', compact('users'));
 	}
 
 	/**
-	 * Show the form for creating a new resource.
+	 * Show the form for creating a new user.
 	 *
-	 * @return \Illuminate\View\View
+	 * @return \Illuminate\View
 	 */
 	public function create() {
-		return view('test.users.create');
+		return view('admin.users.create');
 	}
 
 	/**
-	 * Store a newly created resource in storage.
+	 * Store a newly created user in storage.
 	 *
-	 * @param \Illuminate\Http\Request $request
-	 *
+	 * @param  \Illuminate\Http\Request  $request
 	 * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
 	 */
 	public function store(Request $request) {
+		$validatedAttributes = request()->validate([
+			'name' => 'required|string',
+			'email' => 'required|string',
+			'role' => 'required|integer'
+		]);
 
-		$requestData = $request->all();
-
-		User::create($requestData);
+		$user = User::create($validatedAttributes);
+		abort_if(!$user->exists, 500);
 
 		return redirect('users')->with('flash_message', 'User added!');
 	}
 
 	/**
-	 * Display the specified resource.
+	 * Display the specified user.
 	 *
 	 * @param  int  $id
-	 *
-	 * @return \Illuminate\View\View
+	 * @return \Illuminate\View
 	 */
-	public function show($id) {
-		$user = User::findOrFail($id);
-
-		return view('test.users.show', compact('user'));
+	public function show(int $id) {
+		$user = User::find($id);
+		return view('admin.users.show', compact('user'));
 	}
 
 	/**
-	 * Show the form for editing the specified resource.
+	 * Show the form for editing the specified user.
 	 *
 	 * @param  int  $id
-	 *
-	 * @return \Illuminate\View\View
+	 * @return \Illuminate\View
 	 */
-	public function edit($id) {
-		$user = User::findOrFail($id);
-
-		return view('test.users.edit', compact('user'));
+	public function edit(int $id) {
+		$user = User::find($id);
+		return view('admin.users.edit', compact('user'));
 	}
 
 	/**
-	 * Update the specified resource in storage.
+	 * Update the specified user in storage.
 	 *
-	 * @param \Illuminate\Http\Request $request
-	 * @param  int  $id
-	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  int $id
 	 * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
 	 */
-	public function update(Request $request, $id) {
-
-		$requestData = $request->all();
-
+	public function update(Request $request, int $id) {
 		$user = User::findOrFail($id);
-		$user->update($requestData);
+		$user->update($request->all());
 
 		return redirect('users')->with('flash_message', 'User updated!');
 	}
