dd334ddd8b2ba89f8ab9daeb095746edcc92358e
Katie Arriaga M <katiearriaga@live.com>
2019-03-29 21:10:15 -0600

    Added handling for when the course doesn't have any associated courses

--- resources/views/courses/details.blade.php
+++ resources/views/courses/details.blade.php
@@ -17,63 +17,72 @@
 		</ul>
 		<p><strong>Course schedule:</strong> {{ $course->schedule }}</p>
 		<p><strong>Associated courses:</strong></p>
-		<div class="courses table">
-			<table id="currentCourses" class="ui selectable striped table">
-				<thead class="bluemarket-thead">
-					<tr>
-						<th>Course</th>
-						<th>Professor</th>
-						<th>Schedule</th>
-					</tr>
-				</thead>
-				<tbody>
-						@foreach($course->suppliers as $supplier)
-							<tr class="selectable">
-								<td>
-									<a href="{{ url('courses', $supplier->id) }}">
-										{{ $supplier->name }}
-									</a>
-								</td>
-								<td>
-									<a href="{{ url('courses', $supplier->id) }}">
-										@foreach($supplier->teachers as $teacher)
-											{{ $loop->first ? '' : ', ' }}
-											{{ $teacher->name }}
-										@endforeach
-									</a>
-								</td>
-								<td>
-									<a href="{{ url('courses', $supplier->id) }}">
-										{{ $supplier->schedule }}
-									</a>
-								</td>
-							</tr>
-						@endforeach
-						@foreach($course->clients as $client)
-							<tr class="selectable">
-								<td>
-									<a href="{{ url('courses', $client->id) }}">
-										{{ $client->name }}
-									</a>
-								</td>
-								<td>
-									<a href="{{ url('courses', $client->id) }}">
-										@foreach($client->teachers as $teacher)
-											{{ $loop->first ? '' : ', ' }}
-											{{ $teacher->name }}
-										@endforeach
-									</a>
-								</td>
-								<td>
-									<a href="{{ url('courses', $client->id) }}">
-										{{ $client->schedule }}
-									</a>
-								</td>
-							</tr>
-						@endforeach
-				</tbody>
-			</table>
-		</div>
+		@if (count($course->suppliers) > 0 || count($course->clients) > 0)
+			<div class="courses table">
+				<table id="currentCourses" class="ui selectable striped table">
+					<thead class="bluemarket-thead">
+						<tr>
+							<th>Course</th>
+							<th>Professor</th>
+							<th>Schedule</th>
+						</tr>
+					</thead>
+					<tbody>
+							@foreach($course->suppliers as $supplier)
+								<tr class="selectable">
+									<td>
+										<a href="{{ url('courses', $supplier->id) }}">
+											{{ $supplier->name }}
+										</a>
+									</td>
+									<td>
+										<a href="{{ url('courses', $supplier->id) }}">
+											@foreach($supplier->teachers as $teacher)
+												{{ $loop->first ? '' : ', ' }}
+												{{ $teacher->name }}
+											@endforeach
+										</a>
+									</td>
+									<td>
+										<a href="{{ url('courses', $supplier->id) }}">
+											{{ $supplier->schedule }}
+										</a>
+									</td>
+								</tr>
+							@endforeach
+							@foreach($course->clients as $client)
+								<tr class="selectable">
+									<td>
+										<a href="{{ url('courses', $client->id) }}">
+											{{ $client->name }}
+										</a>
+									</td>
+									<td>
+										<a href="{{ url('courses', $client->id) }}">
+											@foreach($client->teachers as $teacher)
+												{{ $loop->first ? '' : ', ' }}
+												{{ $teacher->name }}
+											@endforeach
+										</a>
+									</td>
+									<td>
+										<a href="{{ url('courses', $client->id) }}">
+											{{ $client->schedule }}
+										</a>
+									</td>
+								</tr>
+							@endforeach
+					</tbody>
+				</table>
+			</div>
+		@else
+			<div class="ui message">
+				<div class="header">
+					No associated courses
+				</div>
+				<p>This course is not currently associated to other courses in Bluemarket</p>
+			</div>
+		@endif
 	</div>
 </div>
 @endsection
