b70d16538615617767300cd1011807546dd179fa
marcegarzab <marcegarzab@gmail.com>
2019-04-01 13:52:12 -0600

    Fix again code climate Cognitive Complexity problem

--- resources/js/searchProjects.js
+++ resources/js/searchProjects.js
@@ -1,10 +1,11 @@
 // npm install --save lodash.debounce OR yarn add lodash.debounce
 import _debounce from 'lodash.debounce';
 
-// Event handler:
+// event handler:
 $('#searchName').on('keyup', _debounce(filterProjects, 200));
 $('#searchTags').on('change', filterProjects);
-// Flatten out the object:
+
+// flatten out the object:
 projects.forEach((project, index) => {
 	project.name = project.name.toLowerCase();
 	project.domid = index;
@@ -12,32 +13,46 @@ projects.forEach((project, index) => {
 		return tag.name;
 	});
 });
+
 // DOM Elements (<projectCard>):
 const $projectCards = $('.ProjectCard-container');
-function filterProjects() {
-	const nameQuery = $('#searchName').val().trim().toLowerCase();
+
+function filterProjectsByTags(projects) {
 	const tagQuery = $('#searchTags').val(); // Returns an array for the select
-	// Get all the projects where name LIKE %namequery%:
-	let projectResults = projects.filter(project => {
-		return project.name.indexOf(nameQuery) > -1; //indexOf returns position of first occurence
-	});
-	// Get the projects that match search tags:
-	const finalResults = projectResults.filter(project => {
+
+	const finalResults = projects.filter(project => {
 		let matchesAllTags = true;
 		for (var i = 0; i < tagQuery.length; i++) {
 			if(project.tags.indexOf(tagQuery[i]) === -1) {
 				matchesAllTags = false;
-				return matchesAllTags;
+				break;
 			}
 		}
 		return matchesAllTags;
 	});
+
+	return finalResults;
+}
+
+function filterProjects() {
+	const nameQuery = $('#searchName').val().trim().toLowerCase();
+
+	// get all the projects where name LIKE %namequery%:
+	let projectResults = projects.filter(project => {
+		return project.name.indexOf(nameQuery) > -1; //indexOf returns position of first occurence
+	});
+
+	// get the projects that match search tags:
+	const finalResults = filterProjectsByTags(projectResults);
+
 	$projectCards.hide();
-	//When no projects where found:
+
+	// no projects found
 	if(finalResults.length == 0){
 		$('.noProjectsMessage').show();
 	}
-	//When projects where found:
+
+	// show found projects
 	finalResults.forEach(project => {
 		$('.noProjectsMessage').hide();
 		$projectCards.eq(project.domid).show();
