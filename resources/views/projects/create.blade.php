5c3787fa14cd6dbfd3758a13c380cdfdab2c0c85
Katie Arriaga M <katiearriaga@live.com>
2019-04-04 00:54:28 -0600

    Changing placeholder image

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -19,7 +19,7 @@
 			<label for="projectImage">Project image</label>
 			<div class="image-container">
 				<div class="imgUploader preview-container">
-					<img src="https://lorempixel.com/400/400" alt="Project image" class="ui small image preview" id="projectImagePreview"/>
+					<img src="https://dummyimage.com/400x400/3498db/ffffff.png&text=B" alt="Project image" class="ui small image preview" id="projectImagePreview"/>
 				</div>
 				<button class="imgUploader ui button primary" type="button">Upload image</button>
 			</div>
