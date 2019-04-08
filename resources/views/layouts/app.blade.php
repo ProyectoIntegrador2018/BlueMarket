3aa8c01198d1907d4171c8c13bf36405b329f350
Katie Arriaga M <katiearriaga@live.com>
2019-04-08 01:47:16 -0500

    Initial navigation dropdown menu for students and teachers

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,14 +14,53 @@
 	<link href="{{ mix('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-	<div class="ui secondary pointing menu bluemarketheader" id="bluemarketheader">
-		<div class="right menu">
+	<div class="ui secondary pointing menu bluemarket-header" id="bluemarket-header">
+		<div class="menu">
 			<a class="item {{ Request::is('/') ? 'active' : '' }}" href="/"> Home </a>
 			<a class="item {{ Request::is('projects') ? 'active' : '' }}" href="{{ action('ProjectController@index') }}">
 				Projects
 			</a>
+		</div>
+		<div class="right menu">
 			@if(Auth::user())
-				<a class="item" href="{{ route('logout') }}">Logout</a>
+				@if(Auth::user()->role ==Config::get('enum.user_roles')['student'])
+					<div class="ui menu bluemarket-header">
+						<a id="add-menu-item" class="browse item">
+							<i class="plus icon"></i>
+							<i class="dropdown icon"></i>
+						</a>
+						<a id="user-menu-item" class="browse item">
+							<img class="ui mini circular image user-avatar" src="{{ isset(Auth::user()->picture_url) ? Auth::user()->picture_url : 'https://dummyimage.com/400x400/3498db/ffffff.png&text=K' }}"/>
+							<i class="dropdown icon"></i>
+						</a>
+					</div>
+					<div id="add-menu" class="ui vertical popup transition hidden">
+						<a class="item user-dropdown-menu" href="{{ url('projects/create') }}">New project</a>
+						<a class="item user-dropdown-menu" href="{{ url('teams/create') }}">New team</a>
+					</div>
+					<div id="user-menu" class="ui vertical popup transition hidden">
+						<a class="item user-dropdown-menu" href="{{ url('users', Auth::id()) }}">My profile</a>
+						<a class="item user-dropdown-menu" href="{{ route('logout') }}">Logout</a>
+					</div>
+				@elseif(Auth::user()->role ==Config::get('enum.user_roles')['teacher'])
+					<div class="ui menu bluemarket-header">
+						<a id="add-menu-item" class="browse item">
+							<i class="plus icon"></i>
+							<i class="dropdown icon"></i>
+						</a>
+						<a id="user-menu-item" class="browse item">
+							<img class="ui mini circular image user-avatar" src="{{ isset(Auth::user()->picture_url) ? Auth::user()->picture_url : 'https://dummyimage.com/400x400/3498db/ffffff.png&text=K' }}"/>
+							<i class="dropdown icon"></i>
+						</a>
+					</div>
+					<div id="add-menu" class="ui vertical popup transition hidden">
+						<a class="item user-dropdown-menu" href="{{ url('courses/create') }}">New course</a>
+					</div>
+					<div id="user-menu" class="ui vertical popup transition hidden">
+						<a class="item user-dropdown-menu" href="{{ url('courses') }}">My courses</a>
+						<a class="item user-dropdown-menu" href="{{ route('logout') }}">Logout</a>
+					</div>
+				@endif
 			@else
 				<a class="item {{ Request::is('login') ? 'active' : '' }}" id="loginBtn" href="{{ route('login') }}">
 					Login
@@ -35,6 +74,17 @@
 	<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.js"></script>
 	<script src="{{ mix('js/inputValidation.js') }}"></script>
+	<script>
+		$('#user-menu-item').popup({
+			popup: $('#user-menu'),
+			on: 'click'
+		});
+
+		$('#add-menu-item').popup({
+			popup: $('#add-menu'),
+			on: 'click'
+		});
+	</script>
 	@yield('scripts')
 </body>
 </html>
