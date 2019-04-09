31f210bfa36bae18c6cf530b0c8417cb7562f41d
Katie Arriaga M <katiearriaga@live.com>
2019-04-09 16:44:26 -0500

    Removed extra line from app service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,7 +17,6 @@ class AppServiceProvider extends ServiceProvider {
 		// Aliasing components
 		Blade::component('components.projectCard', 'projectCard');
 		Blade::component('components.teams.form', 'teamsform');
-
 		if(\App::environment('production')) {
 			URL::forceScheme('https');
 		}
