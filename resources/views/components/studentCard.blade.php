c3598c3eb01e8ef0b9994dfd790cbef434443896
marcegarzab <marcegarzab@gmail.com>
2019-04-09 12:43:59 -0500

    - Added feature to be able to search for students on filter - Create studentCard.blade.php to create the student card framework

---
+++ resources/views/components/studentCard.blade.php
@@ -0,0 +1,22 @@
+<a class="student-card studentCard-container column" href="{{ url('users', $user->id) }}">
+	<div class="ui stackable centered card studentcard">
+		<div class="content">
+			<!--User avatar-->
+			<div>
+				<img class="ui small circular image user-avatar" src="{{ isset($user->picture_url) ? $user->picture_url : 'https://dummyimage.com/400x400/3498db/ffffff.png&text=B' }}">
+			</div>
+			<!--User name-->
+			<!-- TODO: need to make name centered-->
+			<div class="header">{{ $user->name }}</div>
+		</div>
+		<!--User skillset-->
+		@if(isset($user->skillset) && count($user->skillset) > 0)
+			<div class="extra content">
+				<p class="ui sub header">Skills</p>
+				@foreach($user->skillset as $skill)
+					<div class="ui bluemarket-skill label">{{ $skill->name }}</div>
+				@endforeach
+			</div>
+		@endif
+	</div>
+</a>
---
+++ resources/views/students.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.app')
+
+@section('title', 'Users') <!--check if its Users or Students-->
+
+@section('content')
+<div class="padded content">
+	<h1>Students</h1>
+	<!-- Filters -->
+	<form class="ui form">
+		<!-- Search by name -->
+		<div class="field">
+			<label for="searchName">Name</label>
+			<input id="searchName" type="text" name="searchName" placeholder="e.g. Maria Garza">
+		</div>
+		<!-- Search by skills -->
+		<div class="field">
+			<label for="skills">Skills</label>
+			<select id="searchSkills" name="skills" class="ui fluid search dropdown searchSkills" multiple>
+				@foreach ($skills as $skill)
+					<option value="{{ $skill->name }}">{{ $skill->name }}</option>
+				@endforeach
+			</select>
+		</div>
+		<button id="searchButton" type="button" class="ui primary submit button">Search</button>
+	</form>
+	<!-- Message -->
+	<div hidden class="ui message noStudentsMessage">
+		<div class="header">
+			No Students Found
+		</div>
+		<p>No students meet search criteria.</p>
+	</div>
+	<!-- Student cards -->
+	<div class="ui four column stackable grid">
+		@foreach ($users as $user)
+			@studentCard(['user'=> $user])
+			@endstudentCard
+		@endforeach
+	</div>
+</div>
+@endsection
+@section('scripts')
+<script>
+	$('.ui.dropdown').dropdown();
+</script>
+<script src="{{ mix('js/searchStudents.js')}}"></script>
+@endsection
