207a9ea5265ea1c8604b769c62b511ca752b1e7e
Katie Arriaga M <katiearriaga@live.com>
2019-04-20 01:15:11 -0700

    Team details column-tabular layout

--- resources/views/teams/details.blade.php
+++ resources/views/teams/details.blade.php
@@ -4,33 +4,88 @@
 
 @section('content')
 <div class="padded content">
-	<h1>{{ $team->name }}</h1>
-	<div class="team-info">
-		<div class="image-container">
-			<div class="image-uploader preview-container">
-				<img id="preview" src="{{ isset($team->img_url) ? $team->img_url : 'https://dummyimage.com/400x400/3498db/ffffff.png&text=B' }}" alt="Team image" class="ui small image preview"/>
+	<div class="ui stackable grid">
+		<div class="four wide column">
+			<h1 style="text-align: center;">{{ $team->name }}</h1>
+			<img src="{{ isset($team->img_url) ? $team->img_url : 'https://dummyimage.com/400x400/3498db/ffffff.png&text=B' }}" alt="Team image" class="ui small image preview" style="margin: 0 auto; margin-bottom: 20px; border-radius: 15px;"/>
+			<div class="ui stackable grid">
+				<div class="eight wide column">
+					<p><i class="user outline icon"></i><strong>{{ count($team->members) }} Members</strong></p>
+				</div>
+				<div class="eight wide column">
+					<p><i class="file alternate outline icon"></i><strong>{{ count($team->projects) }} Projects</strong></p>
+				</div>
 			</div>
+			<!-- Contact button -->
+			<a class="fluid ui primary button buttonSpace" href="mailto:{{ $team->leader->email }}" style="margin-top: 20px;"><i class="icon envelope"></i>Contact</a>
 		</div>
-		<div class="team-people">
-			<p><strong>People</strong></p>
-			<div class="team-members">
-				@if(isset($team->members))
-					@foreach($team->members as $member)
-						<a href="{{ url('users', $member->id) }}" title="{{ $member->name }}">
-							<img class="ui avatar image" src="{{ isset($member->picture_url) ? $member->picture_url : 'https://dummyimage.com/400x400/3498db/ffffff.png&text=B' }}" />
-						</a>
-					@endforeach
+		<div class="twelve wide column">
+			<div class="ui top attached tabular menu">
+				<a class="active item" data-tab="projects">Projects</a>
+				<a class="item" data-tab="members">Members</a>
+			</div>
+			<div class="ui bottom attached active tab segment" data-tab="projects">
+				@if(isset($team->projects) && count($team->projects) > 0)
+					<div class="ui three column stackable grid">
+						@foreach ($team->projects as $project)
+							@projectCard(['id'=> $project->id,'projectImage' => $project->photo, 'projectName' => $project->name, 'projectShortDescription' => $project->short_description, 'labels' => $project->labels, 'publicMilestone' => 'shipping'])
+							@endprojectCard
+						@endforeach
+					</div>
+				@else
+					<div id="no-projects-msg" class="ui message">
+						<div class="header">
+							No projects found!
+						</div>
+						<p>Looks like this team does not own any projects... yet!</p>
+					</div>
 				@endif
 			</div>
-		</div>
-	</div>
-	<div class="team-projects">
-		<p><strong>Projects</strong></p>
-		<!-- TODO: add project cards -->
-		<div class="ui message">
-			<div class="header"> No projects available </div>
-			<p>Looks like this team has no projects at the moment.</p>
+			<div class="ui bottom attached tab segment" data-tab="members">
+				<div class="{{ count($team->members) <= 0 ? 'hidden' : '' }}">
+					<table class="ui striped table">
+						<tbody>
+							@foreach($team->members as $member)
+								<tr class="selectable">
+									<td>
+										<a href="{{ url('users', $member->id) }}">
+											<img class="ui mini circular image" src="{{ isset($member->picture_url) ? $member->picture_url : 'https://dummyimage.com/400x400/3498db/ffffff.png&text=B' }}" style="display: inline; margin-right: 10px;"/>
+											{{ $member->name }}
+										</a>
+									</td>
+								</tr>
+							@endforeach
+							@foreach($team->members as $member)
+								<tr class="selectable">
+									<td>
+										<a href="{{ url('users', $member->id) }}">
+											<img class="ui mini circular image" src="{{ isset($member->picture_url) ? $member->picture_url : 'https://dummyimage.com/400x400/3498db/ffffff.png&text=B' }}" style="display: inline; margin-right: 10px;"/>
+											{{ $member->name }}
+										</a>
+									</td>
+								</tr>
+							@endforeach
+							@foreach($team->members as $member)
+								<tr class="selectable">
+									<td>
+										<a href="{{ url('users', $member->id) }}">
+											<img class="ui mini circular image" src="{{ isset($member->picture_url) ? $member->picture_url : 'https://dummyimage.com/400x400/3498db/ffffff.png&text=B' }}" style="display: inline; margin-right: 10px;"/>
+											{{ $member->name }}
+										</a>
+									</td>
+								</tr>
+							@endforeach
+						</tbody>
+					</table>
+				</div>
+			</div>
 		</div>
 	</div>
 </div>
+@section("scripts")
+<script>
+	/* Semantic UI setup */
+	$('.menu .item').tab();
+</script>
+@endsection
 @endsection
