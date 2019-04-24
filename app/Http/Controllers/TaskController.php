af3568a27721fc79f1f7ed9dd6b4249a180b93e4
Hector Rincon <hecerinc@hotmail.com>
2019-04-24 00:00:05 -0500

    Add scaffolding for tasks

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller {
+	/**
+	 * Display a listing of the tasks.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function index() {
+		//
+	}
+
+	/**
+	 * Show the form for creating a new task.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function create() {
+		//
+	}
+
+	/**
+	 * Store a newly created task in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return \Illuminate\Http\Response
+	 */
+	public function store(Request $request) {
+		//
+	}
+
+	/**
+	 * Display the specified task.
+	 *
+	 * @param  \App\Task  $task
+	 * @return \Illuminate\Http\Response
+	 */
+	public function show(Task $task) {
+		//
+	}
+
+	/**
+	 * Show the form for editing the specified task.
+	 *
+	 * @param  \App\Task  $task
+	 * @return \Illuminate\Http\Response
+	 */
+	public function edit(Task $task) {
+		//
+	}
+
+	/**
+	 * Update the specified task in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  \App\Task  $task
+	 * @return \Illuminate\Http\Response
+	 */
+	public function update(Request $request, Task $task) {
+		//
+	}
+
+	/**
+	 * Remove the specified task from storage.
+	 *
+	 * @param  \App\Task  $task
+	 * @return \Illuminate\Http\Response
+	 */
+	public function destroy(Task $task) {
+		//
+	}
+}
