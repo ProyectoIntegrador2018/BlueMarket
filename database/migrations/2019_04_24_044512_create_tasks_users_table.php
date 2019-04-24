50546c7e7273dc337267c64f18095c068615b24e
Hector Rincon <hecerinc@hotmail.com>
2019-04-24 00:00:05 -0500

    Add scaffolding for tasks

--- database/migrations/2019_04_24_044512_create_tasks_users_table.php
+++ database/migrations/2019_04_24_044512_create_tasks_users_table.php
@@ -4,20 +4,26 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTeamUserTable extends Migration {
+/*
+ * Creates the Assignments (Task_User) pivot table
+ *
+ */
+class CreateTasksUsersTable extends Migration {
 	/**
 	 * Run the migrations.
 	 *
 	 * @return void
 	 */
 	public function up() {
-		Schema::create('team_user', function (Blueprint $table) {
-			$table->increments('id');
-			$table->integer('team_id')->unsigned();
+		Schema::create('task_user', function (Blueprint $table) {
+			$table->integer('task_id')->unsigned();
 			$table->integer('user_id')->unsigned();
 
-			$table->foreign('team_id')->references('id')->on('teams');
+			$table->primary(['task_id', 'user_id']);
+
+			$table->foreign('task_id')->references('id')->on('tasks');
 			$table->foreign('user_id')->references('id')->on('users');
+
 			$table->timestamps();
 		});
 	}
@@ -28,6 +34,6 @@ class CreateTeamUserTable extends Migration {
 	 * @return void
 	 */
 	public function down() {
-		Schema::dropIfExists('team_user');
+		Schema::dropIfExists('task_user');
 	}
 }
