649a32d913fa6234980d119161e02029dfd85a7f
Ana Karen <anakarenbmak@yahoo.com>
2019-04-24 03:17:37 -0500

    Add initial model and controller for Tasks

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -5,73 +5,83 @@ namespace App\Http\Controllers;
 use App\Task;
 use Illuminate\Http\Request;
 
-class TaskController extends Controller {
-	/**
-	 * Display a listing of the tasks.
-	 *
-	 * @return \Illuminate\Http\Response
-	 */
-	public function index() {
-		//
-	}
+class TaskController extends Controller
+{
+	const TASK_STATUS = 'enum.task_status';
 
-	/**
-	 * Show the form for creating a new task.
-	 *
-	 * @return \Illuminate\Http\Response
-	 */
-	public function create() {
-		//
-	}
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
 
-	/**
-	 * Store a newly created task in storage.
-	 *
-	 * @param  \Illuminate\Http\Request  $request
-	 * @return \Illuminate\Http\Response
-	 */
-	public function store(Request $request) {
-		//
-	}
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
 
-	/**
-	 * Display the specified task.
-	 *
-	 * @param  \App\Task  $task
-	 * @return \Illuminate\Http\Response
-	 */
-	public function show(Task $task) {
-		//
-	}
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
 
-	/**
-	 * Show the form for editing the specified task.
-	 *
-	 * @param  \App\Task  $task
-	 * @return \Illuminate\Http\Response
-	 */
-	public function edit(Task $task) {
-		//
-	}
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Task  $task
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Task $task)
+    {
+        //
+    }
 
-	/**
-	 * Update the specified task in storage.
-	 *
-	 * @param  \Illuminate\Http\Request  $request
-	 * @param  \App\Task  $task
-	 * @return \Illuminate\Http\Response
-	 */
-	public function update(Request $request, Task $task) {
-		//
-	}
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Task  $task
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Task $task)
+    {
+        //
+    }
 
-	/**
-	 * Remove the specified task from storage.
-	 *
-	 * @param  \App\Task  $task
-	 * @return \Illuminate\Http\Response
-	 */
-	public function destroy(Task $task) {
-		//
-	}
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Task  $task
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Task $task)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Task  $task
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Task $task)
+    {
+        //
+    }
 }
