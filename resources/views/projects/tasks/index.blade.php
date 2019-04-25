87f5a2ac33c018b61f88a445b16ecd02432c8650
Katie Arriaga M <katiearriaga@live.com>
2019-04-25 01:35:31 -0500

    Initial view for list of project tasks (task index)

---
+++ resources/views/projects/tasks/index.blade.php
@@ -0,0 +1,106 @@
+<h2>Open</h2>
+<table class="ui selectable very padded stackable yellow table">
+	<thead>
+	<tbody>
+		<tr class="warning">
+			<td>
+				<a class="task-url" href="{{ url('/projects/tasks/details') }}">
+					<p>Upload wireframes to remote workspace</p>
+					<p>Opened <span>2 days ago</span> by {{ $project->team->leader->name }}</p>
+				</a>
+			</td>
+			<td>
+				<a class="task-url" href="{{ url('/projects/tasks/details') }}">
+					<p>Due <span>01/03/2019 11:59PM</span></p>
+					<p><i class="attention icon"></i>Overdue</p>
+				</a>
+			</td>
+		</tr>
+		<tr class="warning">
+			<td>
+				<a class="task-url" href="{{ url('/projects/tasks/details') }}">
+					<p>Upload wireframes to remote workspace</p>
+					<p>Opened <span>2 days ago</span> by {{ $project->team->leader->name }}</p>
+				</a>
+			</td>
+			<td>
+				<a class="task-url" href="{{ url('/projects/tasks/details') }}">
+					<p>Due <span>01/03/2019 11:59PM</span></p>
+					<p><i class="attention icon"></i>Overdue</p>
+				</a>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<a class="task-url" href="{{ url('/projects/tasks/details') }}">
+					<p>Upload wireframes to remote workspace</p>
+					<p>Opened <span>2 days ago</span> by {{ $project->team->leader->name }}</p>
+				</a>
+			</td>
+			<td>
+				<a class="task-url" href="{{ url('/projects/tasks/details') }}">
+					<p>Due <span>01/03/2019 11:59PM</span></p>
+				</a>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<a class="task-url" href="{{ url('/projects/tasks/details') }}">
+					<p>Upload wireframes to remote workspace</p>
+					<p>Opened <span>2 days ago</span> by {{ $project->team->leader->name }}</p>
+				</a>
+			</td>
+			<td>
+				<a class="task-url" href="{{ url('/projects/tasks/details') }}">
+					<p>Due <span>01/03/2019 11:59PM</span></p>
+				</a>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<a class="task-url" href="{{ url('/projects/tasks/details') }}">
+					<p>Upload wireframes to remote workspace</p>
+					<p>Opened <span>2 days ago</span> by {{ $project->team->leader->name }}</p>
+				</a>
+			</td>
+			<td>
+				<a class="task-url" href="{{ url('/projects/tasks/details') }}">
+					<p>Due <span>01/03/2019 11:59PM</span></p>
+				</a>
+			</td>
+		</tr>
+	</tbody>
+</table>
+
+<h2>Closed</h2>
+<table class="ui selectable very padded stackable grey table">
+	<thead>
+	<tbody>
+		<tr>
+			<td>
+				<a class="task-url" href="{{ url('/projects/tasks/details') }}">
+					<p>Upload wireframes to remote workspace</p>
+					<p>Closed <span>2 days ago</span> by {{ $project->team->leader->name }}</p>
+				</a>
+			</td>
+			<td>
+				<a class="task-url" href="{{ url('/projects/tasks/details') }}">
+					<p>Due <span>01/03/2019 11:59PM</span></p>
+				</a>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<a class="task-url" href="{{ url('/projects/tasks/details') }}">
+					<p>Upload wireframes to remote workspace</p>
+					<p>Closed <span>2 days ago</span> by {{ $project->team->leader->name }}</p>
+				</a>
+			</td>
+			<td>
+				<a class="task-url" href="{{ url('/projects/tasks/details') }}">
+					<p>Due <span>01/03/2019 11:59PM</span></p>
+				</a>
+			</td>
+		</tr>
+	</tbody>
+</table>
