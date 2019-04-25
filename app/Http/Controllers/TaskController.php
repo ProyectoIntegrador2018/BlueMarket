78f3909c7f875f57c275dd7aa84d39e6f0f2b917
Ana Karen <anakarenbmak@yahoo.com>
2019-04-24 20:53:46 -0500

    Add validateProject to task controller

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -12,41 +12,45 @@ class TaskController extends Controller
 {
 	const TASK_STATUS = 'enum.task_status';
 
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function index() {
+		//
+	}
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function create() {
 		// TODO: send the project team members to view?
 		return view('tasks.create');
-    }
+	}
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return \Illuminate\Http\Response
+	 */
+	public function store(Request $request) {
 		$attributes = request()->validate([
 			'title' => 'required',
 			'description' => 'required',
 			'dueDate' => 'required', // TODO: check for datetime format
-			'project' => 'required', // TODO: validate for existing user project
+			'project' => 'required|integer'
 		]);
+
+		// Validate the existance of the Project
+		if(!$this->validateProject($attributes['project'])) {
+			return redirect()->back()->withErrors([
+				'project' => 'Invalid Project.'
+			])->withInput();
+		}
 
 		$task = $this->createTask($attributes);
 
@@ -55,53 +59,60 @@ class TaskController extends Controller
 		Auth::user()->save();
 
 		return redirect()->back()->with(compact('task'));
-    }
+	}
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Task  $task
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Task $task)
-    {
-        return view('task.details', compact('task'));
-    }
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  \App\Task  $task
+	 * @return \Illuminate\Http\Response
+	 */
+	public function show(Task $task) {
+		return view('task.details', compact('task'));
+	}
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Task  $task
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Task $task)
-    {
-        return view('task.edit', compact('task'));
-    }
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  \App\Task  $task
+	 * @return \Illuminate\Http\Response
+	 */
+	public function edit(Task $task) {
+		return view('task.edit', compact('task'));
+	}
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Task  $task
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Task $task)
-    {
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  \App\Task  $task
+	 * @return \Illuminate\Http\Response
+	 */
+	public function update(Request $request, Task $task) {
 		$task->update($request->all());
 		return redirect()->back()->with(compact('task'));
-    }
+	}
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Task  $task
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Task $task)
-    {
-        //
-    }
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  \App\Task  $task
+	 * @return \Illuminate\Http\Response
+	 */
+	public function destroy(Task $task) {
+		//
+	}
+
+	private function validateProject($project_id) {
+		Project::find($project_id) == null
+		// Check that all members in the team belong to the course that you're trying to create this project in
+		$project = \App\Project::find($project_id);
+		if(!$project->exists()) {
+			return false;
+		}
+
+		return true;
+	}
 
 	/**
 	 * Create a task with the provided validated attributes
