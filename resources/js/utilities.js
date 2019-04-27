e7114e218c9fdf9a6048e565bafa2bb8a4aad994
Katie Arriaga M <katiearriaga@live.com>
2019-04-27 17:35:34 -0500

    Removed missing function

--- resources/js/utilities.js
+++ resources/js/utilities.js
@@ -7,34 +7,4 @@ window.renderDateTimeAgoOnce = () => {
 	cancel(); // stop real-time rendering
 };
 
-function filterStudentsByName(pattern) {
-	$.ajax({
-		// TODO: update url
-		url: '/users/',
-		method: 'get',
-		headers: {
-			'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-		},
-		data: {
-			'pattern': pattern,
-			'role' : 3 // student
-		},
-		dataType: 'json',
-		success: function (data) {
-			return data;
-		},
-		error: function (data) {
-			// TODO: error handling (need possible errors)
-		}
-	});
-}
-
-function updateUserSearchDropdown() {
-	const pattern = $(".text.filtered").text();
-
-	let dropdownOptions = filterUsersByName(pattern);
-
-	// TODO: update options in dropdown programatically
-}
-
 $(".search.dropdown.user-search").on("keyup", _debounce(updateUserSearchDropdown, 400));
