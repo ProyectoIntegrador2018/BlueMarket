850ca81e8b7718baefb53ba8399023f54aed631f
Katie Arriaga M <katiearriaga@live.com>
2019-04-27 17:59:43 -0500

    Fixed pr issues

--- resources/js/utilities.js
+++ resources/js/utilities.js
@@ -2,7 +2,9 @@ import { render, cancel } from 'timeago.js';
 import _debounce from 'lodash.debounce';
 
 /* global functions */
-window.renderDateTimeAgoOnce = () => {
+function renderDateTimeAgoOnce() {
 	render($(".needs-datetimeago"));
 	cancel(); // stop real-time rendering
 };
+
+window.renderDateTimeAgoOnce = renderDateTimeAgoOnce;
