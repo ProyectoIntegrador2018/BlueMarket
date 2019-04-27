b70ac363489e2aa58bb223d0d0b0d8e48ff1a0b6
Rubén de la Peña <ruben.dlpena@gmail.com>
2019-04-27 17:34:23 -0500

    Code climate 🦁

--- app/Http/Controllers/MilestoneController.php
+++ app/Http/Controllers/MilestoneController.php
@@ -6,9 +6,8 @@ use App\Project;
 use App\Milestone;
 use Illuminate\Http\Request;
 
-class MilestoneController extends Controller
-{
-	const MILESTONE_STATUS = 'enum.milestone_status';
+class MilestoneController extends Controller {
+    const MILESTONE_STATUS = 'enum.milestone_status';
 
     /**
      * Display a listing of the resource.
@@ -16,12 +15,11 @@ class MilestoneController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function index(Request $request)
-    {
-		$projectId = $request->get('projectId');
-		$milestones = Project::findOrFail($projectId)->milestones;
+    public function index(Request $request) {
+        $projectId = $request->get('projectId');
+        $milestones = Project::findOrFail($projectId)->milestones;
 
-		return view('projects.milestones.index', compact('milestones'));
+        return view('projects.milestones.index', compact('milestones'));
     }
 
     /**
@@ -29,8 +27,7 @@ class MilestoneController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
-    {
+    public function create() {
         return view('projects.milestones.create');
     }
 
@@ -40,110 +37,104 @@ class MilestoneController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
-    {
+    public function store(Request $request) {
         $validatedAttributes = request()->validate([
-			'name' => 'required|string',
-			'project_id' => 'required|int',
-		]);
+            'name' => 'required|string',
+            'project_id' => 'required|int',
+        ]);
 
-		$milestone = Milestone::create($validatedAttributes);
-		abort_if(!$milestone->exists, 500);
+        $milestone = Milestone::create($validatedAttributes);
+        abort_if(!$milestone->exists, 500);
 
-		return $milestone;
+        return $milestone;
     }
 
     /**
      * Display the specified resource.
      *
-	 * @param  int  $id
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show(int $id)
-    {
-		$milestone = Milestone::findOrFail($id);
-		return view('projects.milestones.details', compact('milestone'));
+    public function show(int $id) {
+        $milestone = Milestone::findOrFail($id);
+        return view('projects.milestones.details', compact('milestone'));
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-	 * @param  int  $id
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit(int $id)
-    {
-		$milestone = Milestone::findOrFail($id);
-		return view('projects.milestones.edit', compact('milestone'));
+    public function edit(int $id) {
+        $milestone = Milestone::findOrFail($id);
+        return view('projects.milestones.edit', compact('milestone'));
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-	 * @param  int  $id
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, int $id)
-    {
-		$milestone = Milestone::findOrFail($id);
-		$milestone->update($request->all());
+    public function update(Request $request, int $id) {
+        $milestone = Milestone::findOrFail($id);
+        $milestone->update($request->all());
 
-		return $milestone;
+        return $milestone;
     }
 
     /**
      * Remove the specified resource from storage.
      *
-	 * @param  int  $id
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy(int $id)
-    {
-		$milestone = Milestone::findOrFail($id);
+    public function destroy(int $id) {
+        $milestone = Milestone::findOrFail($id);
         $milestone->destroy();
     }
 
     /**
      * Create the default 5 milestones for a project.
      *
-	 * @param  int  $projectId
+     * @param  int  $projectId
      */
-    public static function createDefaultMilestones(int $projectId)
-    {
-		$milestoneIdeation = new Milestone(['name' => 'Ideation',
-			'status' => config(self::MILESTONE_STATUS)['current'],
-			'done_date' => null,
-			'project_id' => $projectId,
-			'previous_milestone_id' => null]);
-		$milestoneIdeation->save();
+    public static function createDefaultMilestones(int $projectId) {
+        $milestoneIdeation = new Milestone(['name' => 'Ideation',
+            'status' => config(self::MILESTONE_STATUS)['current'],
+            'done_date' => null,
+            'project_id' => $projectId,
+            'previous_milestone_id' => null]);
+        $milestoneIdeation->save();
 
-		$milestonertDesign = new Milestone(['name' => 'Design',
-			'status' => null,
-			'done_date' => null,
-			'project_id' => $projectId,
-			'previous_milestone_id' => $milestonIdeation]);
-		$milestonertDesign->save();
+        $milestonertDesign = new Milestone(['name' => 'Design',
+            'status' => null,
+            'done_date' => null,
+            'project_id' => $projectId,
+            'previous_milestone_id' => $milestonIdeation]);
+        $milestonertDesign->save();
 
-		$milestonePlanning = new Milestone(['name' => 'Planning',
-			'status' => null,
-			'done_date' => null,
-			'project_id' => $projectId,
-			'previous_milestone_id' => $milestonDesign]);
-		$milestonePlanning->save();
+        $milestonePlanning = new Milestone(['name' => 'Planning',
+            'status' => null,
+            'done_date' => null,
+            'project_id' => $projectId,
+            'previous_milestone_id' => $milestonDesign]);
+        $milestonePlanning->save();
 
-		$milestonExecution = new Milestone(['name' => 'Execution',
-			'status' => null,
-			'done_date' => null,
-			'project_id' => $projectId,
-			'previous_milestone_id' => $milestonPlanning]);
-		$milestonExecution->save();
+        $milestonExecution = new Milestone(['name' => 'Execution',
+            'status' => null,
+            'done_date' => null,
+            'project_id' => $projectId,
+            'previous_milestone_id' => $milestonPlanning]);
+        $milestonExecution->save();
 
-		$milestonesertTest = new Milestone(['name' => 'Test',
-			'status' => null,
-			'done_date' => null,
-			'project_id' => $projectId,
-			'previous_milestone_id' => $milestonExecution]);
-		$milestonesertTest->save();
+        $milestonesertTest = new Milestone(['name' => 'Test',
+            'status' => null,
+            'done_date' => null,
+            'project_id' => $projectId,
+            'previous_milestone_id' => $milestonExecution]);
+        $milestonesertTest->save();
     }
 }
