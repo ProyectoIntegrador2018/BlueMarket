23563cc272bf2cb95d5f099080ecb2bb92592a26
Katie Arriaga M <katiearriaga@live.com>
2019-04-27 22:05:12 -0500

    Handling timezone differente :tada:

--- resources/js/utilities.js
+++ resources/js/utilities.js
@@ -1,10 +1,13 @@
-import { render, cancel } from 'timeago.js';
+import { format, render, cancel } from 'timeago.js';
 import _debounce from 'lodash.debounce';
 
 /* global functions */
 function renderDateTimeAgoOnce() {
-	render($(".needs-datetimeago"));
-	cancel(); // stop real-time rendering
+	$(".needs-datetimeago").each(function() {
+		let currentDatetime = $(this).data("datetime") + " UTC";
+		let currentDatetimeUTC = new Date(currentDatetime);
+		$(this).html(format(currentDatetimeUTC));
+	});
 };
 
 window.renderDateTimeAgoOnce = renderDateTimeAgoOnce;
