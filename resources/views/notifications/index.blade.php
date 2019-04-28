81785bc04b8a0f49ed1891905d6e76e65caaea19
Katie Arriaga M <katiearriaga@live.com>
2019-04-27 20:14:49 -0500

    Remove padding from notification items

--- resources/views/notifications/index.blade.php
+++ resources/views/notifications/index.blade.php
@@ -27,10 +27,10 @@
 							<div class="ui stackable grid notifications-grid">
 								<div class="twelve wide column">
 									<div class="ui stackable grid">
-										<div class="two wide column">
+										<div class="wide column notification-column user-avatar-column">
 											<img src="{{ isset($user->picture_url) ? $user->picture_url : 'https://dummyimage.com/400x400/3498db/ffffff.png&text=B' }}" alt="Sender image" class="ui tiny image squared-image"/>
 										</div>
-										<div class="ten wide column">
+										<div class="ten wide column notification-column">
 											<p>
 												<a href={{ action('UserController@show', ['id' => $user->id]) }}>
 													{{ $user->name }}
@@ -42,7 +42,7 @@
 												.
 											</p>
 										</div>
-										<div class="four wide column">
+										<div class="four wide column notification-column">
 											<!-- TODO: add datetime rendering classes. Blocked by #188 -->
 											<p class="date">2 minutes ago</p>
 										</div>
@@ -50,10 +50,10 @@
 								</div>
 								<div class="four wide column">
 									<div class="ui padded grid">
-										<div class="eight wide column">
+										<div class="eight wide column notification-column">
 											<p class="notification-option-icon"><i class="link big check circle outline icon green"></i></p>
 										</div>
-										<div class="eight wide column">
+										<div class="eight wide column notification-column">
 											<p class="notification-option-icon"><i class="link big times circle outline icon red"></i></p>
 										</div>
 									</div>
@@ -77,10 +77,10 @@
 							<div class="ui stackable grid notifications-grid">
 								<div class="twelve wide column">
 									<div class="ui stackable grid">
-										<div class="two wide column">
+										<div class="wide column notification-column user-avatar-column">
 											<img src="{{ isset($user->picture_url) ? $user->picture_url : 'https://dummyimage.com/400x400/3498db/ffffff.png&text=B' }}" alt="Sender image" class="ui tiny image squared-image"/>
 										</div>
-										<div class="ten wide column">
+										<div class="ten wide column notification-column">
 											<p>
 												<a href={{ action('UserController@show', ['id' => $user->id]) }}>
 													{{ $user->name }}
@@ -92,7 +92,7 @@
 												.
 											</p>
 										</div>
-										<div class="four wide column">
+										<div class="four wide column notification-column">
 											<!-- TODO: add datetime rendering classes. Blocked by #188 -->
 											<p class="date">2 minutes ago</p>
 										</div>
@@ -100,10 +100,10 @@
 								</div>
 								<div class="four wide column">
 									<div class="ui padded grid">
-										<div class="eight wide column">
+										<div class="eight wide column notification-column">
 											<p class="notification-option-icon"><i class="link big check circle outline icon green"></i></p>
 										</div>
-										<div class="eight wide column">
+										<div class="eight wide column notification-column">
 											<p class="notification-option-icon"><i class="link big times circle outline icon red"></i></p>
 										</div>
 									</div>
