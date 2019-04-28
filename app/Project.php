151e72eaedfb9ac6555764af81880d16509ae84b
Hector Rincon <hecerinc@hotmail.com>
2019-04-27 19:39:30 -0500

    Fix Codeclimate issues

--- app/Project.php
+++ app/Project.php
@@ -81,12 +81,14 @@ class Project extends Model {
 		$is_teacher = $this->course()->first()->teachers()->where('user_id', $id)->exists();
 		$supplier_teachers = $this->course->supplierTeachers();
 		$is_supplier_teacher = false;
+
 		foreach ($supplier_teachers as $teacher) {
 			if($teacher->id == $id) {
 				$is_supplier_teacher = true;
 				break;
 			}
 		}
+
 		return $this->isCollaborator($id) || $is_teacher || $is_supplier_teacher;
 	}
 }
