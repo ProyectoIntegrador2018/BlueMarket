34cf298ce632dbdb348cf460577e700c7faeee17
Katie Arriaga M <katiearriaga@live.com>
2019-04-30 13:05:46 -0500

    Fixed ajax requests

--- resources/views/projects/tasks/create.blade.php
+++ resources/views/projects/tasks/create.blade.php
@@ -8,45 +8,41 @@
 		<form id="new-task-form" class="ui form {{ $errors->any() ? 'error': '' }}" style="width: 100%;">
 			@csrf
 			<div class="ui stackable grid">
-			</div>
-			<div class="row">
-				<div class="ui stackable grid">
-					<div class="sixteen wide column">
-						<!-- Project id -->
-						<input type="hidden" id="project" name="project" value="{{ $project->id }}">
-						<!-- Title -->
-						<div class="field {{ $errors->has('title') ? 'error': '' }}">
-							<label for="title">Title</label>
-							<input type="text" id="title" name="title" placeholder="e.g. Design UI layout" value="{{ old('title') }}">
-						</div>
-						<!-- Due date -->
-						<div class="field {{ $errors->has('dueDate') ? 'error': '' }}">
-							<label for="dueDate">Due date</label>
-							<div class="ui calendar">
-								<div class="ui input left icon">
-									<i class="calendar icon"></i>
-									<input id="dueDate" name="dueDate" type="text" placeholder="e.g. 30/4/2019 5:30 PM" value="{{ old('dueDate') }}">
-								</div>
+				<div class="sixteen wide column">
+					<!-- Project id -->
+					<input type="hidden" id="project" name="project" value="{{ $project->id }}">
+					<!-- Title -->
+					<div class="field {{ $errors->has('title') ? 'error': '' }}">
+						<label for="title">Title</label>
+						<input type="text" id="title" name="title" placeholder="e.g. Design UI layout" value="{{ old('title') }}">
+					</div>
+					<!-- Due date -->
+					<div class="field {{ $errors->has('dueDate') ? 'error': '' }}">
+						<label for="dueDate">Due date</label>
+						<div class="ui calendar">
+							<div class="ui input left icon">
+								<i class="calendar icon"></i>
+								<input id="dueDate" name="dueDate" type="text" placeholder="e.g. 30/4/2019 5:30 PM" value="{{ old('dueDate') }}">
 							</div>
 						</div>
-						<!-- Description -->
-						<div class="field {{ $errors->has('description') ? 'error': '' }}">
-							<label for="description">Description</label>
-							<textarea id="description" name="description" placeholder="e.g. Design the layout for the application in all devices...">{{ old('description') }}</textarea>
-						</div>
 					</div>
-					<div class="sixteen wide column">
-						<!-- Error message -->
-						<div class="ui error message">
-							<h2 class="header">Whoops! Something went wrong.</h2>
-							@if($errors->any())
-								<ul>
-									@foreach ($errors->all() as $error)
-										<li>{{ $error }}</li>
-									@endforeach
-								</ul>
-							@endif
-						</div>
+					<!-- Description -->
+					<div class="field {{ $errors->has('description') ? 'error': '' }}">
+						<label for="description">Description</label>
+						<textarea id="description" name="description" placeholder="e.g. Design the layout for the application in all devices...">{{ old('description') }}</textarea>
+					</div>
+				</div>
+				<div class="sixteen wide column">
+					<!-- Error message -->
+					<div class="ui error message">
+						<h2 class="header">Whoops! Something went wrong.</h2>
+						@if($errors->any())
+							<ul>
+								@foreach ($errors->all() as $error)
+									<li>{{ $error }}</li>
+								@endforeach
+							</ul>
+						@endif
 					</div>
 				</div>
 			</div>
--- resources/views/projects/tasks/index.blade.php
+++ resources/views/projects/tasks/index.blade.php
@@ -2,13 +2,11 @@
 	<div class="eight wide column task-list">
 		<h2>Open tasks</h2>
 		<div class="task">
-			<div class="ui stackable grid">
-				<div class="two wide right aligned column task-status">
-					<span title="Overdue task"><i class="small circle red icon"></i></span>
+			<div class="ui grid">
+				<div class="column">
 					<span title="Open task"><i class="small circle green icon"></i></span>
 				</div>
 				<div class="fourteen wide column">
-					<!-- TODO: task controller show action -->
 					<p class="task-title">Upload wireframes to remote workspace</p>
 					<p>Opened <span>10 days ago</span> by <a href="{{ action('UserController@show', ['id' => $project->team->leader->id]) }}">{{ $project->team->leader->name }}</a></p>
 					<p class="task-due">Due <span>01/03/2019 11:59PM</span></p>
@@ -16,12 +14,11 @@
 			</div>
 		</div>
 		<div class="task">
-			<div class="ui stackable grid">
-				<div class="two wide right aligned column task-status">
+			<div class="ui grid">
+				<div class="column">
 					<span title="Open task"><i class="small circle green icon"></i></span>
 				</div>
 				<div class="fourteen wide column">
-					<!-- TODO: task controller show action -->
 					<p class="task-title">Upload wireframes to remote workspace</p>
 					<p>Opened <span>10 days ago</span> by <a href="{{ action('UserController@show', ['id' => $project->team->leader->id]) }}">{{ $project->team->leader->name }}</a></p>
 					<p class="task-due">Due <span>01/03/2019 11:59PM</span></p>
@@ -29,29 +26,26 @@
 			</div>
 		</div>
 		<div class="task">
-			<div class="ui stackable grid">
-				<div class="two wide right aligned column task-status">
+			<div class="ui grid">
+				<div class="column">
 					<span title="Open task"><i class="small circle green icon"></i></span>
 				</div>
 				<div class="fourteen wide column">
-					<!-- TODO: task controller show action -->
 					<p class="task-title">Upload wireframes to remote workspace</p>
 					<p>Opened <span>10 days ago</span> by <a href="{{ action('UserController@show', ['id' => $project->team->leader->id]) }}">{{ $project->team->leader->name }}</a></p>
-					<p class="task-due">Due <span>01/03/2019 11:59PM</span></p>
+					<p class="task-due overdue">Due <span>01/03/2019 11:59PM</span></p>
 				</div>
 			</div>
 		</div>
 		<div class="task">
-			<div class="ui stackable grid">
-				<div class="two wide right aligned column task-status">
-					<span title="Overdue task"><i class="small circle red icon"></i></span>
+			<div class="ui grid">
+				<div class="column">
 					<span title="Open task"><i class="small circle green icon"></i></span>
 				</div>
 				<div class="fourteen wide column">
-					<!-- TODO: task controller show action -->
 					<p class="task-title">Upload wireframes to remote workspace</p>
 					<p>Opened <span>10 days ago</span> by <a href="{{ action('UserController@show', ['id' => $project->team->leader->id]) }}">{{ $project->team->leader->name }}</a></p>
-					<p class="task-due">Due <span>01/03/2019 11:59PM</span></p>
+					<p class="task-due overdue">Due <span>01/03/2019 11:59PM</span></p>
 				</div>
 			</div>
 		</div>
@@ -59,51 +53,11 @@
 	<div class="eight wide column task-list">
 		<h2>Closed tasks</h2>
 		<div class="task">
-			<div class="ui stackable grid">
-				<div class="two wide right aligned column task-status">
+			<div class="ui grid">
+				<div class="column">
 					<span title="Closed task"><i class="small circle blue icon"></i></span>
 				</div>
 				<div class="fourteen wide column">
-					<!-- TODO: task controller show action -->
-					<p class="task-title">Upload wireframes to remote workspace</p>
-					<p>Opened <span>10 days ago</span> by <a href="{{ action('UserController@show', ['id' => $project->team->leader->id]) }}">{{ $project->team->leader->name }}</a></p>
-					<p class="task-due">Due <span>01/03/2019 11:59PM</span></p>
-				</div>
-			</div>
-		</div>
-		<div class="task">
-			<div class="ui stackable grid">
-				<div class="two wide right aligned column task-status">
-					<span title="Closed task"><i class="small circle blue icon"></i></span>
-				</div>
-				<div class="fourteen wide column">
-					<!-- TODO: task controller show action -->
-					<p class="task-title">Upload wireframes to remote workspace</p>
-					<p>Opened <span>10 days ago</span> by <a href="{{ action('UserController@show', ['id' => $project->team->leader->id]) }}">{{ $project->team->leader->name }}</a></p>
-					<p class="task-due">Due <span>01/03/2019 11:59PM</span></p>
-				</div>
-			</div>
-		</div>
-		<div class="task">
-			<div class="ui stackable grid">
-				<div class="two wide right aligned column task-status">
-					<span title="Closed task"><i class="small circle blue icon"></i></span>
-				</div>
-				<div class="fourteen wide column">
-					<!-- TODO: task controller show action -->
-					<p class="task-title">Upload wireframes to remote workspace</p>
-					<p>Opened <span>10 days ago</span> by <a href="{{ action('UserController@show', ['id' => $project->team->leader->id]) }}">{{ $project->team->leader->name }}</a></p>
-					<p class="task-due">Due <span>01/03/2019 11:59PM</span></p>
-				</div>
-			</div>
-		</div>
-		<div class="task">
-			<div class="ui stackable grid">
-				<div class="two wide right aligned column task-status">
-					<span title="Closed task"><i class="small circle blue icon"></i></span>
-				</div>
-				<div class="fourteen wide column">
-					<!-- TODO: task controller show action -->
 					<p class="task-title">Upload wireframes to remote workspace</p>
 					<p>Opened <span>10 days ago</span> by <a href="{{ action('UserController@show', ['id' => $project->team->leader->id]) }}">{{ $project->team->leader->name }}</a></p>
 					<p class="task-due">Due <span>01/03/2019 11:59PM</span></p>
