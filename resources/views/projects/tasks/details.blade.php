92c89a6d5c19bc11b07bc55e3024d0d0abdf08d1
Katie Arriaga M <katiearriaga@live.com>
2019-04-29 20:14:07 -0500

    Added Project methods to project details view

--- resources/views/projects/tasks/details.blade.php
+++ resources/views/projects/tasks/details.blade.php
@@ -2,12 +2,12 @@
 <div class="ui stackable grid">
 	<div class="ten wide column">
 		<div class="row">
-			<h1>Upload wireframes to the remote repo</h1>
+			<h1 id="task-title">Upload wireframes to the remote repo</h1>
 		</div>
 	</div>
 	<div class="six wide column">
 		<div class = "ui stackable grid">
-			<div class="eight wide column" style="padding-left: 0;">
+			<div id="task-status" class="eight wide column" style="padding-left: 0;">
 				<p><i class="small circle green icon"></i>Open</p>
 			</div>
 		</div>
@@ -15,8 +15,8 @@
 	<div class="ten wide column">
 		<div class="row">
 			<div class="sixteen wide column">
-				<p><strong>Description</strong></p>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Blandit massa enim nec dui nunc mattis. Ac odio tempor orci dapibus ultrices. Tellus pellentesque eu tincidunt tortor aliquam nulla facilisi cras fermentum. Dui id ornare arcu odio ut sem nulla pharetra diam. Aliquam ultrices sagittis orci a scelerisque purus semper eget duis. Enim nunc faucibus a pellentesque. Vitae justo eget magna fermentum iaculis eu non diam phasellus. Facilisis mauris sit amet massa vitae. Placerat in egestas erat imperdiet sed euismod nisi porta. Pretium viverra suspendisse potenti nullam ac tortor. Pellentesque adipiscing commodo elit at imperdiet. Dignissim enim sit amet venenatis urna cursus eget. Tempor orci eu lobortis elementum. Ipsum nunc aliquet bibendum enim. Luctus accumsan tortor posuere ac ut consequat semper viverra nam. Fermentum posuere urna nec tincidunt praesent semper.</p>
+				<p class="task-detail-subtitle">Description</p>
+				<p id="task-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Blandit massa enim nec dui nunc mattis. Ac odio tempor orci dapibus ultrices. Tellus pellentesque eu tincidunt tortor aliquam nulla facilisi cras fermentum. Dui id ornare arcu odio ut sem nulla pharetra diam. Aliquam ultrices sagittis orci a scelerisque purus semper eget duis. Enim nunc faucibus a pellentesque. Vitae justo eget magna fermentum iaculis eu non diam phasellus. Facilisis mauris sit amet massa vitae. Placerat in egestas erat imperdiet sed euismod nisi porta. Pretium viverra suspendisse potenti nullam ac tortor. Pellentesque adipiscing commodo elit at imperdiet. Dignissim enim sit amet venenatis urna cursus eget. Tempor orci eu lobortis elementum. Ipsum nunc aliquet bibendum enim. Luctus accumsan tortor posuere ac ut consequat semper viverra nam. Fermentum posuere urna nec tincidunt praesent semper.</p>
 			</div>
 		</div>
 	</div>
@@ -24,14 +24,14 @@
 		<div class="ui stackable grid">
 			<div class="row">
 				<div class="sixteen wide column">
-					<p><strong>Opened</strong></p>
-					<p>2 days ago by Ana Catalina Arriaga Maldonado</p></p>
+					<p class="task-detail-subtitle">Opened</p>
+					<p><span id="task-closed-date">1 day ago</span> by <span id="task-closed-date">Ana Catalina Arriaga Maldonado</span></p>
 				</div>
 			</div>
 			<div class="row">
 				<div class="sixteen wide column">
-					<p><strong>Closed</strong></p>
-					<p>1 day ago by Ana Catalina Arriaga Maldonado</p></p>
+					<p class="task-detail-subtitle">Closed</p>
+					<p><span id="task-closed-date">1 day ago</span> by <span id="task-closed-date">Ana Catalina Arriaga Maldonado</span></p>
 				</div>
 			</div>
 		</div>
