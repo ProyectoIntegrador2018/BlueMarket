6c7cf4e7c6cece467db62f8253aa1ceedc3d7212
Oscar González <osdagoso@hotmail.com>
2019-05-01 15:03:27 -0500

    Added missing constraint to Eloquent methods

--- app/Team.php
+++ app/Team.php
@@ -23,7 +23,8 @@ class Team extends Model
 		return $this->belongsToMany('App\User', 'team_user', 'team_id', 'user_id')
 			->withPivot('accepted')
 			->withTimestamps()
-			->wherePivot('accepted', config(self::INVITES)['accepted']);
+			->wherePivot('accepted', config(self::INVITES)['accepted'])
+			->where('role', config('enum.user_roles')['student']);
 	}
 
 	public function pending_members() {
@@ -31,6 +32,7 @@ class Team extends Model
 			->withPivot('accepted')
 			->withTimestamps()
 			->wherePivot('accepted', config(self::INVITES)['pending'])
+			->where('role', config('enum.user_roles')['student'])
 			->orderBy('pivot_created_at', 'desc');
 	}
 	public function projects() {
