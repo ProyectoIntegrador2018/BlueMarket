e4e0170bfcf060940d8c7ed6c97a0f77071ae392
Katie Arriaga M <katiearriaga@live.com>
2019-05-13 16:52:53 -0500

    Added handling in project card for when there is no current milestone

--- resources/views/components/projectCard.blade.php
+++ resources/views/components/projectCard.blade.php
@@ -25,19 +25,19 @@
 				@endforeach
 			</div>
 		@endif
-		@if(count($project->milestones) > 0)
-			@php
-				$currentMilestone = $project->milestones->where('status', Config::get('enum.milestone_status')['current'])->first();
-			@endphp
-			@if($currentMilestone != null)
-				<div class="extra content milestone">
+		<div class="extra content milestone">
+			@if(count($project->milestones) > 0)
+				@php
+					$currentMilestone = $project->milestones->where('status', Config::get('enum.milestone_status')['current'])->first();
+				@endphp
+				@if($currentMilestone != null)
 					<p>{{ $currentMilestone->name }}</p>
-				</div>
+				@else
+					<p>On hold</p>
+				@endif
+			@else
+				<p>N/A</p>
 			@endif
-		@else
-			<div class="extra content milestone">
-				<p>On hold</p>
-			</div>
-		@endif
+		</div>
 	</div>
 </a>
