d6381b1f9e09e5e232db51b56ea54258e8b6517e
Katie Arriaga M <katiearriaga@live.com>
2019-05-14 14:26:09 -0500

    Load tasks from DB in the tasks tab

--- resources/js/utilities.js
+++ resources/js/utilities.js
@@ -10,4 +10,23 @@ function renderDateTimeAgoOnce() {
 	});
 };
 
+function utcToLocal() {
+	$(".needs-localdatetime").each(function () {
+		console.log('converting utc to local');
+		let currentDatetimeUTC = $(this).data("datetimeutc") + "Z";
+		let currentDatetime = new Date(currentDatetimeUTC);
+
+		// format the string to YYYY/MM/DD hh:mm:ss
+		let dateString =
+			currentDatetime.getFullYear() + "/" +
+			("0" + (currentDatetime.getMonth() + 1)).slice(-2) + "/" +
+			("0" + currentDatetime.getDate()).slice(-2) + " " +
+			("0" + currentDatetime.getHours()).slice(-2) + ":" +
+			("0" + currentDatetime.getMinutes()).slice(-2) + ":" +
+			("0" + currentDatetime.getSeconds()).slice(-2);
+		$(this).html(dateString);
+	});
+};
+
 window.renderDateTimeAgoOnce = renderDateTimeAgoOnce;
+window.utcToLocal = utcToLocal;
