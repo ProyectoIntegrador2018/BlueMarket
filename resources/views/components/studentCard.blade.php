01a6bd65d225838944fdc7be293980cbdd097db6
marcegarzab <marcegarzab@gmail.com>
2019-05-14 12:14:58 -0500

    Remove resolved toDo

--- resources/views/components/studentCard.blade.php
+++ resources/views/components/studentCard.blade.php
@@ -6,7 +6,6 @@
 				<img class="ui centered small circular image user-avatar-in-card" src="{{ isset($user->picture_url) ? $user->picture_url : 'https://dummyimage.com/400x400/3498db/ffffff.png&text=B' }}">
 			</div>
 			<!--User name-->
-			<!-- TODO: need to make name centered-->
 			<div class="ui header student-card-name">{{ $user->name }}</div>
 		</div>
 		<!--User skillset-->
