88a6eeb908a40b7edd7c47df32de910e1563df43
Katie Arriaga M <katiearriaga@live.com>
2019-05-14 22:00:44 -0500

    Show task details done

--- resources/views/projects/tasks/details.blade.php
+++ resources/views/projects/tasks/details.blade.php
@@ -1,14 +1,14 @@
 <!-- title -->
-<div class="ui stackable grid">
+<div id="task-details" class="ui stackable grid task-details">
 	<div class="ten wide column">
 		<div class="row">
-			<h1 id="task-title">Upload wireframes to the remote repo</h1>
+			<h1 id="task-title"></h1>
 		</div>
 	</div>
 	<div class="six wide column">
 		<div class = "ui stackable grid">
-			<div id="task-status" class="eight wide column" style="padding-left: 0;">
-				<p><i class="small circle green icon"></i>Open</p>
+			<div id="task-status" class="eight wide column" style="padding: 0;">
+				<i class="small circle green icon"></i><p style="display:inline;">Open</p>
 			</div>
 		</div>
 	</div>
@@ -16,23 +16,23 @@
 		<div class="row">
 			<div class="sixteen wide column">
 				<p class="task-detail-subtitle">Description</p>
-				<p id="task-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Blandit massa enim nec dui nunc mattis. Ac odio tempor orci dapibus ultrices. Tellus pellentesque eu tincidunt tortor aliquam nulla facilisi cras fermentum. Dui id ornare arcu odio ut sem nulla pharetra diam. Aliquam ultrices sagittis orci a scelerisque purus semper eget duis. Enim nunc faucibus a pellentesque. Vitae justo eget magna fermentum iaculis eu non diam phasellus. Facilisis mauris sit amet massa vitae. Placerat in egestas erat imperdiet sed euismod nisi porta. Pretium viverra suspendisse potenti nullam ac tortor. Pellentesque adipiscing commodo elit at imperdiet. Dignissim enim sit amet venenatis urna cursus eget. Tempor orci eu lobortis elementum. Ipsum nunc aliquet bibendum enim. Luctus accumsan tortor posuere ac ut consequat semper viverra nam. Fermentum posuere urna nec tincidunt praesent semper.</p>
+				<p id="task-description"></p>
 			</div>
 		</div>
 	</div>
 	<div class="six wide column">
 		<div class="ui stackable grid">
-			<div class="row">
-				<div class="sixteen wide column">
-					<p class="task-detail-subtitle">Opened</p>
-					<p><span id="task-closed-date">1 day ago</span> by <span id="task-closed-date">Ana Catalina Arriaga Maldonado</span></p>
-				</div>
+			<div class="sixteen wide column">
+				<p class="task-detail-subtitle">Due</p>
+				<p id="task-due-date" class="task-due needs-localdatetime"></p>
 			</div>
-			<div class="row">
-				<div class="sixteen wide column">
-					<p class="task-detail-subtitle">Closed</p>
-					<p><span id="task-closed-date">1 day ago</span> by <span id="task-closed-date">Ana Catalina Arriaga Maldonado</span></p>
-				</div>
+			<div class="sixteen wide column">
+				<p class="task-detail-subtitle">Opened</p>
+				<p><span id="task-opened-date" class="needs-localdatetime"></span> by <span id="task-opened-by"></span></p>
+			</div>
+			<div id="task-closed-details" class="sixteen wide column">
+				<p class="task-detail-subtitle">Closed</p>
+				<p><span id="task-closed-date" class="needs-localdatetime"></span> by <span id="task-closed-by"></span></p>
 			</div>
 		</div>
 	</div>
