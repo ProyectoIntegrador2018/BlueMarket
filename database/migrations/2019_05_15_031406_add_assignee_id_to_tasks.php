fe5f5584095365dc5a737cd51d781064e9f5fb53
Ana Karen <anakarenbmak@yahoo.com>
2019-05-14 22:17:36 -0500

    Add assignee to task in db

---
+++ database/migrations/2019_05_15_031406_add_assignee_id_to_tasks.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddAssigneeIdToTasks extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+			$table->integer('assignee_id')->unsigned()->nullable();
+			$table->foreign('assignee_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+			$table->dropColumn('assignee_id');
+        });
+    }
+}
